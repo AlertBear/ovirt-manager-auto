337b31ed5f44f9c9843893d7a28ae73ff411e697
Elena Dolinin <edolinin@redhat.com>
2012-06-15 12:06:40 +0300

    Fixing module name for reports
    Change-Id: I0d6e2b8eecc12139a1331b1dec7afee2874a511d
    Reviewed-on: https://gerrit.eng.lab.tlv.redhat.com/490
    Reviewed-by: Elena Dolinin <edolinin@redhat.com>
    Tested-by: Elena Dolinin <edolinin@redhat.com>

--- test_handler/test_runner.py
+++ test_handler/test_runner.py
@@ -541,7 +541,7 @@ class TestRunner(object):
                 reportStats['group_desc'] = self.groupDescription
         else:
             if opts['has_sub_tests'] == "yes":
-                reportStats['module_name'] = opts['actions'][testCase['test_action']].split('.')[1].capitalize()
+                reportStats['module_name'] = opts['actions'][testCase['test_action']].split('.')[-2].capitalize()
             else:
                 reportStats['module_name'] = None
 
