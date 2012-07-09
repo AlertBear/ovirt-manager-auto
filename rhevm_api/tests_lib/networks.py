43f1722483b4faa88bcd5ddd9d8de9d25c6d99f1
Avi tal <atal@redhat.com>
2012-07-09 07:59:53 +0300

    Fix Network py function plus adding few others
    Fixing entire network py as part of adding/fixing new framework network functionality.
    
    [TRAC ticket: #]
    
    Change-Id: Id0f91c09288210cc9b506d6ce4cc59b4474d0850
    Reviewed-on: https://gerrit.eng.lab.tlv.redhat.com/705
    Tested-by: Avi Tal <atal@redhat.com>
    Reviewed-by: Lukas Bednar <lbednar@redhat.com>
    Reviewed-by: Elena Dolinin <edolinin@redhat.com>

--- rhevm_api/tests_lib/networks.py
+++ rhevm_api/tests_lib/networks.py
@@ -17,23 +17,28 @@
 # Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
 # 02110-1301 USA, or see the FSF site: http://www.fsf.org.
 
-from core_api.apis_utils import getDS
+import logging
+
+from core_api.apis_utils import data_st
 from rhevm_api.utils.test_utils import get_api
 from core_api.apis_exceptions import EntityNotFound
 
 ELEMENT = 'network'
 COLLECTION = 'networks'
-util = get_api(ELEMENT, COLLECTION)
-dcUtil = get_api('data_center', 'datacenters')
-clUtil = get_api('cluster', 'clusters')
+NET_API = get_api(ELEMENT, COLLECTION)
+DC_API = get_api('data_center', 'datacenters')
+CL_API = get_api('cluster', 'clusters')
 
-Network = getDS('Network')
-IP = getDS('IP')
+logger = logging.getLogger(__package__ + __name__)
 
 
 def _prepareNetworkObject(**kwargs):
-    
-    net = Network()
+    '''
+    preparing logical network object
+    Author: edolinin, atal
+    return: logical network data structure object
+    '''
+    net = data_st.Network()
 
     if 'name' in kwargs:
         net.set_name(kwargs.get('name'))
@@ -44,37 +49,31 @@ def _prepareNetworkObject(**kwargs):
     if 'stp' in kwargs:
         net.set_stp(kwargs.get('stp'))
 
-    if 'display' in kwargs:
-        netUpd.set_display(kwargs.get('display'))
-
     if 'data_center' in kwargs:
         dc = kwargs.get('data_center')
-        net.set_data_center(dcUtil.find(dc))
+        net.set_data_center(DC_API.find(dc))
 
     address = kwargs.get('address')
     netmask = kwargs.get('netmask')
     gateway = kwargs.get('gateway')
+    if (address or netmask or gateway) is not None:
+        ip = {}
+        if address is not None:
+            ip['address'] = address
+        if netmask is not None:
+            ip['netmask'] = netmask
+        if gateway is not None:
+            ip['gateway'] = gateway
+        net.set_ip(data_st.IP(**ip))
 
-    if address or netmask or gateway:
-        ip = IP(address=address, netmask=netmask, gateway=gateway)
-        net.set_ip(ip)
+    if 'vlan_id' in kwargs:
+        net.set_vlan(data_st.VLAN(id=kwargs.get('vlan_id')))
 
-    vlan = kwargs.get('vlan_id')
-    if vlan:
-        net.set_vlan(id=vlan_id)
+    if 'usages' in kwargs:
+        net.set_usages(data_st.Usages(usage=kwargs.get('usages').split(',')))
 
-    usages = kwargs.get('usages')
-    if usages:
-        netUsages = Usages()
-        for usage in usages:
-            netUsages.add_usage(usage)
-        net.set_usages(netUsages)
-
-
-    mtu = kwargs.get('mtu')
-    if mtu:
-        net.set_mtu(mtu)
-        
+    if 'mtu' in kwargs:
+        net.set_mtu(kwargs.get('mtu'))
 
     return net
 
@@ -85,45 +84,28 @@ def addNetwork(positive, **kwargs):
     Author: edolinin
     Parameters:
        * name - name of a new network
+       * description - new network description (if relevant)
        * data_center - data center name where a new network should be added
        * address - network ip address
        * netmask - network ip netmask
        * gateway - network ip gateway
-       * stp - support stp true/false (note: true/false as a strings) 
+       * stp - support stp true/false (note: true/false as a strings)
        * vlan_id - network vlan id
-       * display - set as display or not (true/false)
+       * usages - a string contain list of usages separated by commas 'VM,DIPLAY'.
+       * mtu - and integer to overrule mtu on the related host nic..
     Return: status (True if network was added properly, False otherwise)
     '''
 
-    net = _prepareNetworkObject(**kwargs)
-    net, status = util.create(net, positive)
+    net_obj = _prepareNetworkObject(**kwargs)
+    res, status = NET_API.create(net_obj, positive)
 
     return status
-
-
-def findNetwork(network, data_center=None):
-    found_net = False
-    # FIXME: data_center is needed in some cases untill BZ#741111 will be solved
-    if data_center:
-        dc_obj = dcUtil.find(data_center)
-        # NOTE: data_center isn't mandatory in order not to break
-        # other related functions
-        nets = util.get(absLink=False)
-        for net in nets:
-            if net.get_data_center().get_id() == dc_obj.get_id() and \
-            net.get_name().lower() == network.lower():
-                found_net = True
-                break
-    else:
-        return True
-
-    return found_net
 
 
 def updateNetwork(positive, network, **kwargs):
     '''
     Description: update existing network
-    Author: edolinin
+    Author: edolinin, atal
     Parameters:
        * network - name of a network that should be updated
        * name - new network name (if relevant)
@@ -134,37 +116,57 @@ def updateNetwork(positive, network, **kwargs):
        * description - new network description (if relevant)
        * stp - new network support stp (if relevant). (note: true/false as a strings)
        * vlan_id - new network vlan id (if relevant)
-       * display - new network display value (if relevant)
+       * usages - a string contain list of usages separated by commas 'VM,DIPLAY'.
+                    should contain all usages every update.
+                    a missing usage will be deleted!
+       * mtu - and integer to overrule mtu on the related host nic..
     Return: status (True if network was updated properly, False otherwise)
     '''
-    if not findNetwork(network, kwargs.get('data_center')):
-        # found_net is a flag that suppose to be true if founded network
-        # else exist the function with EntityNotFound
-        raise EntityNotFound
 
-    net = util.find(network)
-    netUpd = _prepareNetworkObject(**kwargs)
-    netUpd, status = util.update(net, netUpd, positive)
-    
+    net = findNetwork(network, kwargs.get('data_center'))
+    net_update = _prepareNetworkObject(**kwargs)
+    res, status = NET_API.update(net, net_update, positive)
+
     return status
 
 
-def removeNetwork(positive, network, data_center=None,):
+def removeNetwork(positive, network, data_center=None):
     '''
     Description: remove existing network
-    Author: edolinin
+    Author: edolinin, atal
     Parameters:
        * network - name of a network that should be removed
        * data_center - In case more then one network with the same name exists.
     Return: status (True if network was removed properly, False otherwise)
     '''
-    if not findNetwork(network, data_center):
-        # found_net is a flag that suppose to be true if founded network
-        # else exist the function with EntityNotFound
-        raise EntityNotFound
 
-    net = util.find(network)
-    return util.delete(net, positive)
+    net = findNetwork(network, data_center)
+    return NET_API.delete(net, positive)
+
+
+def findNetwork(network, data_center=None):
+    '''
+    Description: Find desired network among other networks with same name
+    findNetwork is needed due to BZ#741111.
+    Author: atal
+    Parameters:
+        * network - network name
+        * data_center - DC which given network is a member of.
+        note: DC isn't mandatory in order not to break API
+    return: network object in case of succeed, raise EntityNotFound in case
+    of Failure
+    '''
+
+    if data_center is not None:
+        dc_obj = DC_API.find(data_center)
+        nets = NET_API.get(absLink=False)
+        for net in nets:
+            if net.get_data_center().get_id() == dc_obj.get_id() and \
+            net.get_name().lower() == network.lower():
+                return net
+        raise EntityNotFound('%s network does not exists!' % network)
+    else:
+        return NET_API.find(network)
 
 
 def removeMultiNetworks(positive, networks):
@@ -181,42 +183,119 @@ def removeMultiNetworks(positive, networks):
     return True
 
 
+def _prepareClusterNetworkObj(**kwargs):
+    '''
+    preparing cluster network object
+    Author: edolinin, atal
+    return: logical network data structure object for cluster
+    '''
+    net = kwargs.get('net', data_st.Network())
+
+    if 'usages' in kwargs:
+        net.set_usages(data_st.Usages(usage=kwargs.get('usages').split(',')))
+
+    if 'required' in kwargs:
+        net.set_required(kwargs.get('required'))
+
+    if 'display' in kwargs:
+        net.set_display(kwargs.get('display'))
+
+    return net
+
+
 def getClusterNetwork(cluster, network):
 
-    clusterObj = clUtil.find(cluster)
-    return util.getElemFromElemColl(clusterObj, network)
+    clusterObj = CL_API.find(cluster)
+    return CL_API.getElemFromElemColl(clusterObj, network, 'networks', 'network')
 
 
 def getClusterNetworks(cluster):
 
-    clusterObj = clUtil.find(cluster)
-    return util.getElemFromLink(clusterObj, get_href=True)
+    clusterObj = CL_API.find(cluster)
+    return CL_API.getElemFromLink(clusterObj, link_name='networks', attr='network', get_href=True)
 
 
-def addNetworkToCluster(positive, network, cluster):
+def findNetworkByCluster(network, cluster):
+    '''
+    Design to compare cluster DC with network DC in order to
+    workaround BZ#741111
+    Author: atal
+    Parameters:
+        * network - network name
+        * cluster - cluster name
+    return: network object in case of success, raise EntityNotFound in case
+    of Failure
+    '''
+    nets = NET_API.get(absLink=False)
+    cluster_obj = CL_API.find(cluster)
+    cluster_dc_id = cluster_obj.get_data_center().get_id()
+
+    for net in nets:
+        if cluster_dc_id == net.get_data_center().get_id() and \
+        network == net.get_name():
+            return net
+    raise EntityNotFound('%s network does not exists!' % network)
+
+
+def addNetworkToCluster(positive, network, cluster, **kwargs):
     '''
     Description: attach network to cluster
-    Author: edolinin, atal
+    Author: atal
     Parameters:
        * network - name of a network that should be attached
        * cluster - name of a cluster to attach to
+       * required - boolean, decide if network should be required by cluster..
+       * usages - a string contain list of usages separated by comma
+       'VM,DIPLAY'. should contain all usages every update.
+       a missing usage will be deleted!
+       * display - deprecated. boolean, a spice display network.
     Return: status (True if network was attached properly, False otherwise)
     '''
-    # FIXME: this function is a workaround for BZ#741111
-    network_objs = util.get(absLink=False)
-    cluster_obj = clUtil.find(cluster)
-    cluster_cd_id = cluster_obj.get_data_center().get_id()
-    clNetworks = getClusterNetworks(cluster)
-   
-    if network_objs is None:
-        raise EntityNotFound('Found Empty networks element')
 
-    for network_obj in network_objs:
-        if cluster_cd_id == network_obj.get_data_center().get_id() and \
-        network == network_obj.get_name():
-            net, status = util.create(network_obj, positive, collection=clNetworks)
-            return status
-    return False
+    kwargs.update(net=findNetworkByCluster(network, cluster))
+    net = _prepareClusterNetworkObj(**kwargs)
+    cluster_nets = getClusterNetworks(cluster)
+    res, status = NET_API.create(net, positive,
+                                     collection=cluster_nets)
+
+    return status
+
+
+def updateClusterNetwork(positive, cluster, network, **kwargs):
+    '''
+    Description: update network to cluster
+    Author: atal
+    Parameters:
+       * network - name of a network that should be attached
+       * cluster - name of a cluster to attach to
+       * required - boolean, decide if network should be required by cluster..
+       * usages - a string contain list of usages separated by
+       commas 'VM,DIPLAY'. should contain all usages every update.
+       a missing usage will be deleted!
+       * display - deprecated. boolean, a spice display network.
+    Return: status (True if network was attached properly, False otherwise)
+    '''
+
+    net = getClusterNetwork(cluster, network)
+    net_update = _prepareClusterNetworkObj(**kwargs)
+    res, status = NET_API.update(net, net_update, positive)
+
+    return status
+
+
+def removeNetworkFromCluster(positive, network, cluster):
+    '''
+    Description: detach network from cluster
+    Author: edolinin, atal
+    Parameters:
+       * network - name of a network that should be detached
+       * cluster - name of a cluster to detach from
+    Return: status (True if network was detached properly, False otherwise)
+    '''
+
+    net_obj = getClusterNetwork(cluster, network)
+
+    return NET_API.delete(net_obj, positive)
 
 
 def addMultiNetworksToCluster(positive, networks, cluster):
@@ -249,26 +328,7 @@ def removeMultiNetworksFromCluster(positive, networks, cluster):
     return True
 
 
-def removeNetworkFromCluster(positive, network, cluster):
-    '''
-    Description: detach network from cluster
-    Author: edolinin, atal
-    Parameters:
-       * network - name of a network that should be detached
-       * cluster - name of a cluster to detach from
-    Return: status (True if network was detached properly, False otherwise)
-    '''
-
-    net_obj = getClusterNetwork(cluster, network)
-
-    if net_obj:
-        return util.delete(net_obj, positive)
-    else:
-        util.logger.error("Network {0} is not found at cluster {1}".format(network,
-        cluster))
-        return False
-
-
+# FIXME: need to check if this function is being used else just remove.
 def getNetworkConfig(positive, cluster, network, datacenter=None, tag=None):
     '''
      Validate Datacenter/Cluster network configurations/existence.
@@ -292,7 +352,7 @@ def getNetworkConfig(positive, cluster, network, datacenter=None, tag=None):
     # validate cluster network related to the given datacenter
     if datacenter:
         try:
-            dcObj = dcUtil.find(datacenter)
+            dcObj = DC_API.find(datacenter)
         except EntityNotFound:
             return False, {'value': None}
 
@@ -316,6 +376,8 @@ def getNetworkConfig(positive, cluster, network, datacenter=None, tag=None):
     return False, tag
 
 
+# FIXME: method is using only for checking status. need to change to a more
+# simple method
 def validateNetwork(positive, cluster, network, tag, val):
     status, output = getNetworkConfig(positive, cluster, network, tag=tag)
     if not status:
@@ -325,6 +387,7 @@ def validateNetwork(positive, cluster, network, tag, val):
     return True
 
 
+# FIXME: change to use conf file for vlan networks name
 def addNetworksVlans(positive, prefix, vlans, data_center):
     '''
     Adding multiple networks with vlan according to the given prefix and range
@@ -333,11 +396,12 @@ def addNetworksVlans(positive, prefix, vlans, data_center):
         * prefix - the prefix for the network name
         * vlans - a list vlan ids
         * date_center - the DataCenter name
-    return True with new nics name list or False with empty list 
+    return True with new nics name list or False with empty list
     '''
     nics = []
     for vlan in vlans.split(','):
         nics.append(prefix + str(vlan))
-        if not addNetwork(positive, prefix + str(vlan), data_center, vlan_id=vlan):
+        net_name = prefix + str(vlan)
+        if not addNetwork(positive, name=net_name, data_center=data_center, vlan_id=vlan):
             return False, {'nets': None}
     return True, {'nets': nics}
