33cd7cc142a74169465aec8b6fb6880aeaa14c27
Larisa Ustalov <lustalov@redhat.com>
2012-08-26 16:39:10 +0300

    ticket #1312 - added function to insert fake data to report views for long-time tests
    Change-Id: Ie0ec927a31cce10bfb8ed5fa106115eeeac693d2
    Reviewed-on: https://gerrit.eng.lab.tlv.redhat.com/1524
    Tested-by: Larisa Ustalov <lustalov@redhat.com>
    Reviewed-by: Elena Dolinin <edolinin@redhat.com>
    Reviewed-by: Lukas Bednar <lbednar@redhat.com>
    Reviewed-by: Larisa Ustalov <lustalov@redhat.com>

---
+++ art/rhevm_api/utils/db_utils.py
@@ -0,0 +1,240 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2012 Red Hat, Inc.
+#
+# This is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation; either version 2.1 of
+# the License, or (at your option) any later version.
+#
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this software; if not, write to the Free
+# Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+# 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+
+import art.test_handler.settings as settings
+from utilities.postgresConnection import Postgresql
+from utilities import errors
+from datetime import datetime, timedelta, date
+from dateutil import tz
+import re
+from logging import getLogger
+
+logger = getLogger('db_utils')
+
+HISTORY_DB_NAME = 'ovirt_engine_history'
+
+DAILY = 'daily'
+HOURLY = 'hourly'
+SAMPLES = 'samples'
+
+HOUR_INTERVAL = 'h'
+DAY_INTERVAL = 'd'
+MONTH_INTERVAL = 'm'
+
+MAX_SAMPLES = 48 * 60 # 48 hours once a minute
+MAX_HOURS = 60 * 24   # 2 months once an hour
+MAX_DAYS = 2 * 365    # 2 years once a day
+
+class EntityIsNotFoundInDBError(errors.GeneralException):
+    message = 'Entity of this type is not found in the configuration table.'
+
+class WrongTimeIntervalFormatError(errors.GeneralException):
+    message = 'Wrong time interval format.'
+
+class WrongTimeIntervalTypeError(errors.GeneralException):
+    message = 'Wrong time interval type.'
+    message += 'Can be one of the following: {0}, {1}, {2}'.format(
+            HOUR_INTERVAL, DAY_INTERVAL, MONTH_INTERVAL)
+
+
+class HistoryDB(object):
+
+    TABLES = {
+        'config'     : 'history_configuration',
+        'host': {
+            'config' : 'host_configuration',
+            DAILY    : 'host_daily_history',
+            HOURLY   : 'host_hourly_history',
+            SAMPLES  : 'host_samples_history',
+        },
+        'vm': {
+            'config' : 'vm_configuration',
+            DAILY    : 'vm_daily_history',
+            HOURLY   : 'vm_hourly_history',
+            SAMPLES  : 'vm_samples_history',
+        }
+    }
+    DATETIME_COL = 'history_datetime'
+
+    def __init__(self):
+        host = settings.opts.get('host', 'localhost')
+        user = settings.opts.get('history_db_user', 'postgres')
+        passw = settings.opts.get('history_db_passw', None)
+        dbname = settings.opts.get('history_db_name', HISTORY_DB_NAME)
+        self._dbConn = Postgresql(host=host, user=user, password=passw,
+                             dbname=dbname)
+
+    def __enter__(self):
+        self._dbConn.connect()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self._dbConn.close()
+
+    def __insert(self, table, columns, values):
+        '''
+        Run SQL insert statement.
+        '''
+        adjVals = []
+        for val in values:
+            if isinstance(val, str):
+                adjVals.append('\'%s\'' % val)
+            else:
+                adjVals.append(str(val))
+        sql = 'INSERT INTO {0} ({1}) VALUES ({2});'.format(table,
+                ','.join(columns), ','.join(adjVals))
+        return self._dbConn.execute(sql)
+
+    def __update(self, table, propPattern, propValue, columns, values):
+        '''
+        Run SQL update statement.
+        '''
+        if isinstance(columns, list):
+            updateList = ['{0}=\'{1}\''.format(columns[i], values[i]) for i in range (0,
+                len(columns))]
+            updateStr = ','.join(updateList)
+        else:
+            updateStr = '{0}=\'{1}\''.format(columns, values)
+        sql = 'UPDATE {0} SET {1} WHERE {2} = \'{3}\''.format(table, updateStr,
+                                            propPattern, propValue)
+        return self._dbConn.execute(sql)
+
+    def __getEntityId(self, entity, name):
+        '''
+        Get entity Id by name.
+        '''
+        table = self.TABLES[entity]['config']
+        sql = 'SELECT DISTINCT {0}_id FROM {1} WHERE {2}_name = \'{3}\' AND delete_date IS NULL'.format(
+            entity, table, entity, name)
+        out = self._dbConn.query(sql)
+        if not out:
+            raise EntityIsNotFoundInDBError(entity, name, table)
+        return out[-1][0]
+
+    def __calculateRecordsCount(self, interval, type):
+        interval_patt = re.compile('(\d+)(\w)', re.I)
+        m = interval_patt.search(interval)
+        if not m:
+            raise WrongTimeIntervalFormatError(interval)
+        interval_val = int(m.group(1))
+        interval_type = m.group(2).lower()
+        if interval_type not in (HOUR_INTERVAL, DAY_INTERVAL, MONTH_INTERVAL):
+            raise WrongTimeIntervalTypeError(interval_type)
+        if type == SAMPLES:
+            if interval_type == HOUR_INTERVAL:
+                interval_val *= 60
+            elif interval_type == DAY_INTERVAL:
+                interval_val *= 60 * 24
+            elif interval_type == MONTH_INTERVAL:
+               interval_val = MAX_SAMPLES
+            if interval_val > MAX_SAMPLES:
+                interval_val = MAX_SAMPLES
+        elif type == HOURLY:
+            if interval_type == DAY_INTERVAL:
+                interval_val *= 24
+            elif interval_type == MONTH_INTERVAL:
+                interval_val *= 30 * 24
+            if interval_val > MAX_HOURS:
+                interval_val = MAX_HOURS
+        elif type == DAILY:
+            if interval_type == HOUR_INTERVAL:
+                interval_val /= 24
+            elif interval_type == MONTH_INTERVAL:
+                interval_val *= 30
+            if interval_val > MAX_DAYS:
+                interval_val = MAX_DAYS
+        return interval_val
+
+    def __getStartTime(self, type):
+        if type == DAILY:
+            return date.today()
+        return datetime.now(tz.tzlocal())
+            
+    def __getTimestamp(self, type, startTime, cnt):
+        if type == SAMPLES:
+            start = startTime - timedelta(minutes=cnt)
+        elif type == HOURLY:
+            start = startTime - timedelta(hours=cnt)
+        elif type == DAILY:
+            start = startTime - timedelta(days=cnt)
+        return start.isoformat().replace('T', ' ')
+
+    def __updateHistoryConfiguration(self, start, end):
+        sql = 'SELECT var_datetime from %s where var_name=\'firstSync\';' % \
+                self.TABLES['config']
+        out = self._dbConn.query(sql)
+        if start < out[0][0].isoformat():
+            self.__update(self.TABLES['config'], 'var_name', 'firstSync',
+                            'var_datetime', start)
+        self.__update(self.TABLES['config'], 'var_name', 'lastDayAggr',
+                        'var_datetime', end)
+        self.__update(self.TABLES['config'], 'var_name', 'lastHourAggr',
+                        'var_datetime', end)
+
+    def fillTable(self, entity, name, type, properties, values, interval):
+        '''
+        Insert data to DB table
+        '''
+        try:
+            table = self.TABLES[entity][type]
+        except KeyError:
+            logger.error(
+                'Table with %s data for %s is not supported.' %
+                (entity, type))
+            return False
+        try:
+            entityId = self.__getEntityId(entity, name)
+            recordCnt = self.__calculateRecordsCount(interval, type)
+            startTime = self.__getStartTime(type)
+            entityStatus = 1
+            props = [self.DATETIME_COL, '%s_id' % entity, '%s_status' % entity]
+            for prop in properties.split(','):
+                props.append(prop)
+            for ind in range (0, recordCnt):
+                tstamp = self.__getTimestamp(type, startTime, ind)
+                vals = [tstamp, entityId, entityStatus]
+                for val in values.split(','):
+                    vals.append(int(val))
+                self.__insert(table, props, vals)
+            self.__updateHistoryConfiguration(tstamp, startTime.isoformat())
+            logger.info('%d records inserted to %s table' % (recordCnt, table))
+        except Exception:
+            logger.exception('Failed to insert data to the table %s' % table)
+            return False
+        return True
+
+
+def forgeHistoryData(entity, name, type, properties, values, interval):
+    '''
+    Insert fake data to DB table
+    Parameters:
+        - entity - type of entity(host, vm, storage_domain)
+        - name - entity name
+        - type - history type (hourly, daily, samples)
+        - properties - property names (comma separated)
+        - values - property values (comma separated)
+        - interval - time interval (Nh - N hours, Nd - N days, Nm - N month)
+    Return:
+        True/False
+    '''
+    with HistoryDB() as dbh:
+        rc = dbh.fillTable(entity, name, type, properties, values, interval)
+        return rc
+
