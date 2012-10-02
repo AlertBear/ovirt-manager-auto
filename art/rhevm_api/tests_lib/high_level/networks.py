b216ea180f3b066c5c007e8e9672efb98ade532b
Avi tal <atal@redhat.com>
2012-10-02 17:26:15 +0200

    Split networks high and low level functions
    Split networks high level functions from low level module
    
    [TRAC ticket: n/a]
    
    Change-Id: I798827ccf6b99a0a40baa5188cf3e1dbad1b7bbd
    Reviewed-on: https://gerrit.eng.lab.tlv.redhat.com/2398
    Tested-by: Avi Tal <atal@redhat.com>
    Reviewed-by: Elena Dolinin <edolinin@redhat.com>
    Reviewed-by: Avi Tal <atal@redhat.com>

---
+++ art/rhevm_api/tests_lib/high_level/networks.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python
+
+# Copyright (C) 2010 Red Hat, Inc.
+#
+# This is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation; either version 2.1 of
+# the License, or (at your option) any later version.
+#
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this software; if not, write to the Free
+# Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+# 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+
+import os
+import logging
+import re
+
+from art.rhevm_api.tests_lib.low_level import networks as nets
+from art.core_api.apis_exceptions import EntityNotFound
+from utilities.utils import readConfFile
+from art.core_api import is_action
+
+ELEMENTS = os.path.join(os.path.dirname(__file__), '../../../conf/elements.conf')
+ENUMS = readConfFile(ELEMENTS, 'RHEVM Enums')
+
+logger = logging.getLogger(__package__ + __name__)
+
+
+@is_action()
+def addMultipleVlanedNetworks(networks, data_center, **kwargs):
+    '''
+    Adding multiple networks with vlan according to the given prefix and range
+    Author: atal
+    Parameters:
+        * networks - a list of vlaned networks with their vlan suffix
+        * date_center - the DataCenter name
+        * kwargs - all arguments related to basic addNetwork 
+    return True with new nics name list or False with empty list
+    '''
+
+    for network in networks:
+        vlan = re.search(r'(\d+)', network)
+        if not nets.addNetwork('True', name=network, data_center=data_center,
+                               vlan_id=vlan.group(0), **kwargs):
+            return False
+    return True
+
+
+# FIXME: need to check if this function is being used else just remove.
+@is_action()
+def getNetworkConfig(positive, cluster, network, datacenter=None, tag=None):
+    '''
+     Validate Datacenter/Cluster network configurations/existence.
+     This function tests the network configured parameters. we look for
+     networks link under cluster even though we check the DC network
+     configurations only because cluster networks related to main networks href.
+     Author: atal
+     Parameters:
+        * cluster - cluster name
+        * network - network name
+        * datacenter - data center name. in order to check if the given
+                       network exists in the DC.
+        * tag - the tag we are looking to validate
+     return: True and value of the given filed, otherwise False and None
+    '''
+    try:
+        netObj = nets.getClusterNetwork(cluster, network)
+    except EntityNotFound:
+        return False, {'value': None}
+
+    # validate cluster network related to the given datacenter
+    if datacenter:
+        try:
+            dcObj = nets.DC_API.find(datacenter)
+        except EntityNotFound:
+            return False, {'value': None}
+
+        # return False means that given datacenter doesn't contain the given network
+        if dcObj.get_id() != netObj.get_data_center().get_id():
+            return False, {'value': None}
+
+    if tag:
+        if hasattr(netObj, tag):
+            attrValue = None
+            if tag == 'status':
+                attrValue = getattr(netObj.get_status(), 'state')
+            else:
+                attrValue = getattr(netObj, tag)
+            return True, {'value': attrValue}
+
+    # in canse we only like to check if the network exists or not.
+    if netObj:
+        return True, {'value': netObj.get_name()}
+
+    return False, tag
+
+
+# FIXME: method is using only for checking status. need to change to a more
+# simple method
+@is_action()
+def validateNetwork(positive, cluster, network, tag, val):
+    status, output = getNetworkConfig(positive, cluster, network, tag=tag)
+    if not status:
+        return False
+    if str(output['value']).lower() != str(val).lower():
+        return False
+    return True
+
+
+@is_action()
+def removeMultiNetworks(positive, networks):
+    '''
+    Remove Multiple networks from cluster
+    Author: atal
+    Parameters:
+        * networks- a list of networks
+    return True/False
+    '''
+    for net in networks:
+        if not nets.removeNetwork(positive, net):
+            return False
+    return True
--- art/rhevm_api/tests_lib/low_level/networks.py
+++ art/rhevm_api/tests_lib/low_level/networks.py
@@ -173,21 +173,6 @@ def findNetwork(network, data_center=None):
         return NET_API.find(network)
 
 
-@is_action()
-def removeMultiNetworks(positive, networks):
-    '''
-    Remove Multiple networks from cluster
-    Author: atal
-    Parameters:
-        * networks- a list of networks
-    return True/False
-    '''
-    for net in networks:
-        if not removeNetwork(positive, net):
-            return False
-    return True
-
-
 def _prepareClusterNetworkObj(**kwargs):
     '''
     preparing cluster network object
@@ -335,67 +320,6 @@ def removeMultiNetworksFromCluster(positive, networks, cluster):
     for net in networks:
         if not removeNetworkFromCluster(positive, net, cluster):
             return False
-    return True
-
-
-# FIXME: need to check if this function is being used else just remove.
-@is_action()
-def getNetworkConfig(positive, cluster, network, datacenter=None, tag=None):
-    '''
-     Validate Datacenter/Cluster network configurations/existence.
-     This function tests the network configured parameters. we look for
-     networks link under cluster even though we check the DC network
-     configurations only because cluster networks related to main networks href.
-     Author: atal
-     Parameters:
-        * cluster - cluster name
-        * network - network name
-        * datacenter - data center name. in order to check if the given
-                       network exists in the DC.
-        * tag - the tag we are looking to validate
-     return: True and value of the given filed, otherwise False and None
-    '''
-    try:
-        netObj = getClusterNetwork(cluster, network)
-    except EntityNotFound:
-        return False, {'value': None}
-
-    # validate cluster network related to the given datacenter
-    if datacenter:
-        try:
-            dcObj = DC_API.find(datacenter)
-        except EntityNotFound:
-            return False, {'value': None}
-
-        # return False means that given datacenter doesn't contain the given network
-        if dcObj.get_id() != netObj.get_data_center().get_id():
-            return False, {'value': None}
-
-    if tag:
-        if hasattr(netObj, tag):
-            attrValue = None
-            if tag == 'status':
-                attrValue = getattr(netObj.get_status(), 'state')
-            else:
-                attrValue = getattr(netObj, tag)
-            return True, {'value': attrValue}
-
-    # in canse we only like to check if the network exists or not.
-    if netObj:
-        return True, {'value': netObj.get_name()}
-
-    return False, tag
-
-
-# FIXME: method is using only for checking status. need to change to a more
-# simple method
-@is_action()
-def validateNetwork(positive, cluster, network, tag, val):
-    status, output = getNetworkConfig(positive, cluster, network, tag=tag)
-    if not status:
-        return False
-    if str(output['value']).lower() != str(val).lower():
-        return False
     return True
 
 
