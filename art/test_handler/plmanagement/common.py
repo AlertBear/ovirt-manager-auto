adef3ce54f9a3e4d689623892eb3bc9ec91c668a
Lukas Bednar <lbednar@redhat.com>
2012-10-15 14:53:45 +0200

    fixed typo in common module
    Change-Id: If04179c2f2bad2a1595c9caa9726fb6367a512df
    Reviewed-on: https://gerrit.eng.lab.tlv.redhat.com/2638
    Tested-by: Lukas Bednar <lbednar@redhat.com>
    Reviewed-by: Elena Dolinin <edolinin@redhat.com>

--- art/test_handler/plmanagement/common.py
+++ art/test_handler/plmanagement/common.py
@@ -6,7 +6,7 @@ DC_TYPE = 'data_center_type'
 def get_vds_section(config):
     vds_section = PARAMETERS
     dc_val = config[PARAMETERS][DC_TYPE].upper()
-    if dc_val != 'NONE' and dc_val in conf:
+    if dc_val != 'NONE' and dc_val in config:
         vds_section = dc_val
     return vds_section
 
