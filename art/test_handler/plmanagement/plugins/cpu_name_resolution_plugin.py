2a8c94c5c0c23152f64e8719269ecd2d82cc8b95
Elena Dolinin <edolinin@redhat.com>
2012-10-25 08:31:20 +0200

    Adding pydocs to plugins which should be exposed in documentation
    Change-Id: I01a3382e2e0b9c976966c17b0cdb0038f8b7cb9c
    Reviewed-on: https://gerrit.eng.lab.tlv.redhat.com/2857
    Reviewed-by: Elena Dolinin <edolinin@redhat.com>
    Tested-by: Elena Dolinin <edolinin@redhat.com>

--- art/test_handler/plmanagement/plugins/cpu_name_resolution_plugin.py
+++ art/test_handler/plmanagement/plugins/cpu_name_resolution_plugin.py
@@ -1,3 +1,22 @@
+"""
+--------------------------
+Cpu Name Resolution Plugin
+--------------------------
+
+This plugin adjusts config section for cpu_name attribute.
+It finds the maximum compatible cpu_name to use for the vds configured and
+puts it into PARAMETERS.cpu_name value.
+
+CLI Options:
+------------
+    --with-cpu-name-resolution enable plugin
+
+Configuration File Options:
+----------------------
+    [CPU_NAME_RESOLUTION]
+    enabled   to enable the plugin (true/false)
+"""
+
 import re
 
 from art.test_handler.plmanagement import (Component, implements, get_logger,
