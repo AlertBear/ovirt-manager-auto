793aec9a422a52c0920eac4b25a04ad059073910
Lukas Bednar <lbednar@redhat.com>
2013-01-15 10:51:15 +0100

    packaging: added python-futures dependency
    Change-Id: I2d3c40268743c5c5338d1c2b103669150fa96165
    Reviewed-on: https://gerrit.eng.lab.tlv.redhat.com/4169
    Reviewed-by: Elena Dolinin <edolinin@redhat.com>
    Reviewed-by: Jakub Libosvar <jlibosva@redhat.com>
    Tested-by: Lukas Bednar <lbednar@redhat.com>

--- setup.py
+++ setup.py
@@ -61,6 +61,8 @@ DEPS = [
         ]
 if sys.version_info[0] == 2 and sys.version_info[1] < 7:
     DEPS.append('python-argparse')
+if sys.version_info[0] < 3:
+    DEPS.append('python-futures')
 
 
 SCRIPT = """\
