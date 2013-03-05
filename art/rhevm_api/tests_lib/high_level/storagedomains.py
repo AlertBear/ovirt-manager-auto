e3c5b0afcb0a042156e2d53b3c0f13e9ca79c890
Jakub Libosvar <jlibosva@redhat.com>
2013-03-05 13:11:57 +0100

    ART: added storage format to high level storage functions
    Also did some cosmetic fixes
    
    Change-Id: I93906fb3e21c96484ca438fc8e65c3becfb7cb93
    Reviewed-on: https://gerrit.eng.lab.tlv.redhat.com/6867
    Tested-by: Meni Yakove <myakove@redhat.com>
    Reviewed-by: Elena Dolinin <edolinin@redhat.com>

--- art/rhevm_api/tests_lib/high_level/storagedomains.py
+++ art/rhevm_api/tests_lib/high_level/storagedomains.py
@@ -17,20 +17,21 @@
 # Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
 # 02110-1301 USA, or see the FSF site: http://www.fsf.org.
 
-import os
 import logging
 
 from art.rhevm_api.tests_lib.low_level import storagedomains
 from art.core_api import is_action
+from art.rhevm_api.tests_lib.low_level import storagedomains
 from art.test_handler.settings import opts
 import art.test_handler.exceptions as errors
 
 ENUMS = opts['elements_conf']['RHEVM Enums']
 
-logger = logging.getLogger(__package__ + __name__)
+logger = logging.getLogger(__name__)
 
 @is_action()
-def addISCSIDataDomain(host, storage, data_center, lun, lun_address, lun_target, lun_port=3260):
+def addISCSIDataDomain(host, storage, data_center, lun, lun_address,
+                       lun_target, lun_port=3260, storage_format=None):
     '''
     positive flow for adding ISCSI Storage including all the necessary steps
     Author: atal
@@ -42,34 +43,40 @@ def addISCSIDataDomain(host, storage, data_center, lun, lun_address, lun_target,
         * lun_address - iscsi server address
         * lun_target - name of lun target in iscsi server
         * lun_port - lun port
+        * storage_format - storage format version (v1/v2/v3)
     return True if succeeded, False otherwise
     '''
 
-    if not storagedomains.iscsiDiscover('True', host, lun_address):
-        logger.error('Failed to discover lun address %s from %s' % (lun_address, host))
+    if not storagedomains.iscsiDiscover(True, host, lun_address):
+        logger.error('Failed to discover lun address %s from %s' %
+                     (lun_address, host))
         return False
 
-    if not storagedomains.iscsiLogin('True', host, lun_address, lun_target):
-        logger.error('Failed to login %s on target %s from %s' % (lun_address, lun_target, host))
+    if not storagedomains.iscsiLogin(True, host, lun_address, lun_target):
+        logger.error('Failed to login %s on target %s from %s' %
+                     (lun_address, lun_target, host))
         return False
 
-    if not storagedomains.addStorageDomain('True', host=host, name=storage,
+    if not storagedomains.addStorageDomain(True, host=host, name=storage,
                                     type=ENUMS['storage_dom_type_data'],
                                     storage_type=ENUMS['storage_type_iscsi'],
                                     lun=lun, lun_address=lun_address,
-                                    lun_target=lun_target, lun_port=lun_port):
-        logger.error('Failed to add %s to %s' % (lun_address, host))
+                                    lun_target=lun_target, lun_port=lun_port,
+                                    storage_format=storage_format):
+        logger.error('Failed to add (%s, %s, %s) to %s' % (lun_address,
+                     lun_target, lun, host))
         return False
 
-    status = storagedomains.attachStorageDomain('True', data_center, storage,
-                                                'True')
+    status = storagedomains.attachStorageDomain(True, data_center, storage,
+                                                True)
 
     return status and storagedomains.activateStorageDomain(True, data_center,
                                  storage)
 
 
 @is_action()
-def addNFSDomain(host, storage, data_center, address, path, sd_type=ENUMS['storage_dom_type_data']):
+def addNFSDomain(host, storage, data_center, address, path,
+                 sd_type=ENUMS['storage_dom_type_data'], storage_format=None):
     '''
     positive flow for adding NFS Storage including all the necessary steps
     Author: atal
@@ -80,16 +87,19 @@ def addNFSDomain(host, storage, data_center, address, path, sd_type=ENUMS['stora
         * address - nfs server address
         * path - path for nfs mount
         * sd_type - type of storage domain: data, iso or export
+        * storage_format - storage format version (v1/v2/v3)
     return True if succeeded, False otherwise
     '''
-    if not storagedomains.addStorageDomain('True', host=host, name=storage,
-                                    type=sd_type, storage_type=ENUMS['storage_type_nfs'],
-                                    address=address, path=path):
-        logger.error('Failed to add %s to %s' % (address, host))
+    if not storagedomains.addStorageDomain(True, host=host, name=storage,
+                                    type=sd_type,
+                                    storage_type=ENUMS['storage_type_nfs'],
+                                    address=address, path=path,
+                                    storage_format=storage_format):
+        logger.error('Failed to add %s:%s to %s' % (address, path, host))
         return False
 
-    status = storagedomains.attachStorageDomain('True', data_center, storage,
-                                                'True')
+    status = storagedomains.attachStorageDomain(True, data_center, storage,
+                                                True)
     return status and storagedomains.activateStorageDomain(True, data_center,
                                  storage)
 
@@ -206,3 +216,4 @@ def __create_posixfs_storages(datacenter, host, storage_conf):
     TODO
     """
     raise NotImplementedError("Not implemented yet!")
+
