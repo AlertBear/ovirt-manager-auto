ce666b733ae3c1a7740005807fe9fde20e41c29e
Jakub Libosvar <jlibosva@redhat.com>
2013-03-05 12:29:43 +0100

    Storage domain live upgrade test
    Change-Id: I30b7b95b2020af3166b604589fe18313d94a19ee
    Reviewed-on: https://gerrit.eng.lab.tlv.redhat.com/6925
    Tested-by: Jakub Libosvar <jlibosva@redhat.com>
    Reviewed-by: Attila Darazs <adarazs@redhat.com>

---
+++ art/tests/rhevm/unittests/storage/storage_domain_upgrade/config.py
@@ -0,0 +1,27 @@
+"""
+Config module for storage domain upgrade
+"""
+
+__test__ = False
+
+from art.test_handler.settings import opts
+from . import ART_CONFIG
+
+# Name of the test
+TESTNAME = "storage_domain_upgrade"
+
+PARAMETERS = ART_CONFIG['PARAMETERS']
+#NFS = ART_CONFIG['NFS']
+#ISCSI = ART_CONFIG['ISCSI']
+
+# Workers for thread pool
+MAX_WORKERS = PARAMETERS.get('max_workers', 16)
+
+TMP_CLUSTER_NAME = 'tmp_cluster'
+
+# Enums
+ENUMS = opts['elements_conf']['RHEVM Enums']
+
+DC_VERSIONS = PARAMETERS.as_list('dc_versions')
+DC_UPGRADE_VERSIONS = PARAMETERS.as_list('dc_upgrade_versions')
+STORAGE_TYPES = PARAMETERS.as_list('storage_types')
