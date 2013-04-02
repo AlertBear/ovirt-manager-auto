7506904543d76d0264b140c3f868c3d5f97d5c13
Genadi Chereshnya <gcheresh@redhat.com>
2013-04-02 17:31:44 +0300

    Functions for creating and deleting networks in DC, Cluster and Host
    Adding 2 high level functions:
    1) To create networks on DC/Cluster and Host
    2) To delete networks from the setup
    3) Updating the networks.py to support pep8
    
    Change-Id: I47d8863a3e8007aa53f76cfc58a3edee2d2576c0
    Reviewed-on: https://gerrit.eng.lab.tlv.redhat.com/7390
    Tested-by: Genadi Chereshnya <gcheresh@redhat.com>
    Reviewed-by: Larisa Ustalov <lustalov@redhat.com>
    Reviewed-by: Elena Dolinin <edolinin@redhat.com>

--- art/rhevm_api/tests_lib/high_level/networks.py
+++ art/rhevm_api/tests_lib/high_level/networks.py
@@ -30,6 +30,7 @@ from art.test_handler.settings import opts
 ENUMS = opts['elements_conf']['RHEVM Enums']
 
 logger = logging.getLogger(__package__ + __name__)
+CONNECTIVITY_TIMEOUT = 60
 
 
 @is_action()
@@ -59,7 +60,8 @@ def getNetworkConfig(positive, cluster, network, datacenter=None, tag=None):
      Validate Datacenter/Cluster network configurations/existence.
      This function tests the network configured parameters. we look for
      networks link under cluster even though we check the DC network
-     configurations only because cluster networks related to main networks href.
+     configurations only because cluster networks related
+     to main networks href.
      Author: atal
      Parameters:
         * cluster - cluster name
@@ -81,7 +83,8 @@ def getNetworkConfig(positive, cluster, network, datacenter=None, tag=None):
         except EntityNotFound:
             return False, {'value': None}
 
-        # return False means that given datacenter doesn't contain the given network
+        # return False means that given datacenter
+        #doesn't contain the given network
         if dcObj.get_id() != netObj.get_data_center().get_id():
             return False, {'value': None}
 
@@ -125,4 +128,103 @@ def removeMultiNetworks(positive, networks):
     for net in networks:
         if not nets.removeNetwork(positive, net):
             return False
+    return True
+
+
+def createAndAttachNetworkSN(data_center, cluster, host, auto_nics=[],
+                             network_dict={}):
+    '''
+        Function that creates and attach the network to the:
+        a) DC, b) Cluster, c) Host with SetupNetworks
+        Author: gcheresh
+        Parameters:
+        * data_center - DC name
+        * cluster - Cluster name
+        * host - remote machine ip address or fqdn
+        * auto_nics - a list of nics
+        * network_dict - dictionary of dictionaries for the following
+          net parameters:
+            * logical network name as the key for the following:
+                * nic - interface to create the network on
+                * usages - VM/non-VM or display network
+                * vlan_id - list of values, each value for specific network
+                * mtu - list of values, each value for specific network
+                * required - required/non-required network
+                * bond - bond name to create
+                * slaves - interfaces that the bond will be composed from
+                * mode - the mode of the bond
+        Return: True value if succeeded in creating and adding network list
+                to DC/Cluster and Host
+    '''
+
+    net_obj = []
+    for key in network_dict.keys():
+        logger.info("Adding network to DC")
+        bond = network_dict[key].get('bond')
+        if not lnet.addNetwork(True, name=key, data_center=data_center,
+                               usages=network_dict[key].get('usages', 'vm'),
+                               vlan_id=network_dict[key].get('vlan_id'),
+                               mtu=network_dict[key].get('mtu')):
+            logger.error("Cannot add network to DC")
+            return False
+
+        logger.info("Adding network to Cluster")
+        if not lnet.addNetworkToCluster(True, network=key, cluster=cluster,
+                                        required=network_dict[key].
+                                        get('required')):
+            logger.error("Cannot add network to Cluster")
+            return False
+
+        if not bond:
+            logger.info("Generating network object for SetupNetwork ")
+            rc, out = lhosts.genSNNic(nic=network_dict[key]['nic'],
+                                      network=key,
+                                      vlan=network_dict[key].get('vlan_id'))
+            if not rc:
+                logger.error("Cannot generate network object")
+                return False
+            net_obj.append(out['host_nic'])
+        if bond:
+            logger.info("Generating network object for bond ")
+            rc, out = lhosts.genSNBond(name=network_dict[key]['bond'],
+                                       network=key,
+                                       slaves=network_dict[key].get('slaves'),
+                                       mode=network_dict[key].get('mode'))
+            if not rc:
+                logger.error("Cannot generate network object ")
+                return False
+            net_obj.append(out['host_nic'])
+    try:
+        lhosts.sendSNRequest(True, host=host, nics=net_obj,
+                             auto_nics=auto_nics, check_connectivity='true',
+                             connectivity_timeout=60, force='false')
+    except Exception as ex:
+        logger.error("SendSNRequest failed %s", ex)
+        return False
+    return True
+
+
+def removeNetFromSetup(host, auto_nics=['eth0'], network=[]):
+    '''
+        Function that removes networks from the host, Cluster and DC:
+        Author: gcheresh
+        Parameters:
+        * host - remote machine ip address or fqdn
+        * auto_nics - a list of nics
+        * network - list of networks to remove
+        Return: True value if succeeded in deleting network
+                from Host, Cluster DC
+    '''
+    try:
+        lhosts.sendSNRequest(True, host=host,
+                             auto_nics=auto_nics,
+                             check_connectivity='true',
+                             connectivity_timeout=CONNECTIVITY_TIMEOUT,
+                             force='false')
+        lhosts.commitNetConfig(True, host=host)
+        for index in range(len(network)):
+            lnet.removeNetwork(True, network=config.NETWORKS[index])
+    except Exception as ex:
+        logger.error("Remove Network from setup failed %s", ex, exc_info=True)
+        return False
     return True
