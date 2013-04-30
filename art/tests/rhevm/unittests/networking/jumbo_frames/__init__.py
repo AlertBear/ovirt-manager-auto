97dbe35a1bca1b44515fe771834755755ec26551
Genadi Chereshnya <gcheresh@redhat.com>
2013-04-30 15:32:20 +0300

    Changing the __init__ to use prepareSetup function
    Change-Id: I24cb9182dccffbc93d9e11116cd82a8db0453e27
    Reviewed-on: https://gerrit.eng.lab.tlv.redhat.com/7930
    Tested-by: Genadi Chereshnya <gcheresh@redhat.com>
    Reviewed-by: Larisa Ustalov <lustalov@redhat.com>
    Reviewed-by: Genadi Chereshnya <gcheresh@redhat.com>

--- art/tests/rhevm/unittests/networking/jumbo_frames/__init__.py
+++ art/tests/rhevm/unittests/networking/jumbo_frames/__init__.py
@@ -5,6 +5,7 @@ Jumbo Frames test
 import logging
 from art.rhevm_api.tests_lib.low_level.storagedomains import\
     createDatacenter, cleanDataCenter
+from art.rhevm_api.tests_lib.high_level.networks import prepareSetup
 from art.test_handler.exceptions import DataCenterException
 
 logger = logging.getLogger("Jumbo")
@@ -17,17 +18,21 @@ def setup_package():
     Prepare environment
     """
     import config
-    if not createDatacenter(positive=True, hosts=config.HOSTS[0],
-                            cpuName=config.CPU_NAME, username='root',
-                            password=config.HOSTS_PW,
-                            datacenter=config.DC_NAME,
-                            storage_type=config.STORAGE_TYPE,
-                            cluster=config.CLUSTER_NAME,
-                            version=config.VERSION,
-                            lun_address=config.LUN_ADDRESS,
-                            lun_target=config.LUN_TARGET,
-                            luns=config.LUN, lun_port=3260):
-        raise DataCenterException("Cannot create setup")
+    if not prepareSetup(hosts=config.HOSTS[0], cpuName=config.CPU_NAME,
+                        username='root', password=config.HOSTS_PW,
+                        datacenter=config.DC_NAME,
+                        storageDomainName=config.DC_NAME + '_data_domain0',
+                        storage_type=config.STORAGE_TYPE,
+                        cluster=config.CLUSTER_NAME,
+                        lun_address=config.LUN_ADDRESS,
+                        lun_target=config.LUN_TARGET,
+                        luns=config.LUN, version=config.VERSION,
+                        cobblerAddress=config.COBBLER_ADDRESS,
+                        cobblerUser=config.COBBLER_USER,
+                        cobblerPasswd=config.COBBLER_PASSWORD,
+                        vm_password=config.HOSTS_PW, vm_flag=False,
+                        template_flag=False):
+        raise NetworkException("Cannot create setup")
 
 
 def teardown_package():
--- art/tests/rhevm/unittests/networking/jumbo_frames/config.py
+++ art/tests/rhevm/unittests/networking/jumbo_frames/config.py
@@ -5,9 +5,12 @@ Jumbo Frames config module
 __test__ = False
 
 from . import ART_CONFIG
+from art.test_handler.settings import opts
+from concurrent.futures import ThreadPoolExecutor
 
 TEST_NAME = "Jumbo"
 PARAMETERS = ART_CONFIG['PARAMETERS']
+ENUMS = opts['elements_conf']['RHEVM Enums']
 STORAGE_TYPE = PARAMETERS['data_center_type']
 
 basename = PARAMETERS.get('TEST_NAME', 'test')
@@ -27,8 +30,19 @@ LUN = PARAMETERS.as_list('lun')[0]
 
 HOST_NICS = PARAMETERS.as_list('host_nics')
 NETWORKS = PARAMETERS.as_list('networks')
+VLAN_NETWORKS = PARAMETERS.as_list('vlan_networks')
 VLAN_ID = PARAMETERS.as_list('vlan_id')
 BOND = PARAMETERS.as_list('bond')
 VM_NAME = PARAMETERS.as_list('vm_name')
 TEMPLATE_NAME = PARAMETERS['template_name']
 VM_OS = PARAMETERS['vm_os']
+
+COBBLER_ADDRESS = PARAMETERS['cobbler_address']
+COBBLER_USER = PARAMETERS['cobbler_user']
+COBBLER_PASSWORD = PARAMETERS['cobbler_passwd']
+
+DISK_TYPE = ENUMS['disk_type_system']
+DISPLAY_TYPE = ENUMS['display_type_spice']
+NIC_TYPE_VIRTIO = ENUMS['nic_type_virtio']
+NIC_TYPE_RTL8139 = ENUMS['nic_type_rtl8139']
+NIC_TYPE_E1000 = ENUMS['nic_type_e1000']
