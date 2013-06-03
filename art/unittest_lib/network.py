0990d8e23f87ac42d7cd1203746f6f702fb55949
Elena Dolinin <edolinin@redhat.com>
2013-06-03 09:46:43 +0300

    Revert "Get function and run it for given time util success or timeout."
    This reverts commit 19a026df4d8cdcb9b9ead4e060dc2bcac5221cfc
    
    Change-Id: Ia7f50429ec8ecdf2b651b344340f05481ab98b9d
    Reviewed-on: https://gerrit.eng.lab.tlv.redhat.com/8337
    Reviewed-by: Elena Dolinin <edolinin@redhat.com>
    Tested-by: Elena Dolinin <edolinin@redhat.com>

--- art/unittest_lib/network.py
+++ art/unittest_lib/network.py
@@ -17,13 +17,9 @@
 # Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
 # 02110-1301 USA, or see the FSF site: http://www.fsf.org.
 
-import time
 import logging
-
 logger = logging.getLogger(__name__)
 DEAFULT_HOST_NICS_NUM = 4
-TIMEOUT = 60
-SLEEP = 1
 
 
 def skipBOND(case_list, host_nics):
@@ -39,32 +35,3 @@ def skipBOND(case_list, host_nics):
         for case in case_list:
             case.__test__ = False
             logger.info("%s is skipped, host cannot run BOND test case" % case)
-
-
-def WaitForFuncStatus(result=True, timeout=TIMEOUT, sleep=SLEEP, func=None,
-                      *args, **kwargs):
-    '''
-    Description: Get function and run it for given time util success or
-                 timeout.
-    **Author**: myakove
-    **Parameters**:
-        * *result* - Expected result from func (True or False)
-        * *timeout* - timeout for running the loop
-        * *sleep* - Time to sleep between retries
-        * *func* - function (*args and **kwargs are from function)
-    Example (calling updateNic function)::
-        WaitForFuncStatus(result=True, timeout=60, sleep=1,
-                          func=updateNic, positive=True,
-                          vm=config.VM_NAME[0], nic="vnic3",
-                          network=config.VLAN_NETWORKS[1],
-                          plugged='false')
-    '''
-    time_out = timeout
-    while timeout > 0:
-        if func(*args, **kwargs) == result:
-            return True
-        timeout -= sleep
-        time.sleep(sleep)
-    logger.error("(%s) return incorrect status after %s seconds" %
-                (func.__name__, time_out))
-    return False
