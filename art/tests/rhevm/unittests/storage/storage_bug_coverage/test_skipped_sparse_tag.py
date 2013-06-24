5648c23e9471d1a259b667f452417f3c45d9d1fb
Katarzyna Jachim <kjachim@redhat.com>
2013-06-24 13:52:01 +0200

    Test exposing BZ 960430
    Test scenario:
    * tries to create a disk via REST API without specifying 'sparse' tag
    
    Change-Id: I2478e03ce0b5caaa2962005b5dfaa7de0177d2bb
    Reviewed-on: https://gerrit.eng.lab.tlv.redhat.com/8659
    Tested-by: Katarzyna Jachim <kjachim@redhat.com>
    Reviewed-by: Jakub Libosvar <jlibosva@redhat.com>
    Reviewed-by: Gadi Ickowicz <gickowic@redhat.com>
    Reviewed-by: Meital Bourvine <mbourvin@redhat.com>

---
+++ art/tests/rhevm/unittests/storage/storage_bug_coverage/test_skipped_sparse_tag.py
@@ -0,0 +1,61 @@
+"""
+Test exposing BZ 960430
+
+TCMS plan: https://tcms.engineering.redhat.com/plan/9583
+"""
+
+import logging
+from nose.tools import istest
+from unittest import TestCase
+
+from art.rhevm_api.tests_lib.high_level import datacenters
+from art.rhevm_api.tests_lib.low_level import storagedomains
+from art.rhevm_api.tests_lib.low_level import disks
+
+import config
+
+LOGGER = logging.getLogger(__name__)
+GB = 1024 * 1024 * 1024
+
+ENUMS = config.ENUMS
+
+
+def setup_module():
+    """ creates datacenter, adds hosts, clusters, storages according to
+    the config file
+    """
+    datacenters.build_setup(
+        config=config.PARAMETERS, storage=config.PARAMETERS,
+        storage_type=config.DATA_CENTER_TYPE, basename=config.BASENAME)
+
+
+def teardown_module():
+    """ removes created datacenter, storages etc.
+    """
+    storagedomains.cleanDataCenter(True, config.DATA_CENTER_NAME)
+
+
+class TestCase276003(TestCase):
+    """ Test exposing https://bugzilla.redhat.com/show_bug.cgi?id=960430
+    Tries to create a disk via REST API without specifying 'sparse' tag.
+
+    https://tcms.engineering.redhat.com/case/284324/?from_plan=9583
+    """
+    __test__ = True
+    tcms_plan_id = '9583'
+    tcms_test_case = '284324'
+
+    @istest
+    def create_raw_disk_without_sparse_tag_test(self):
+        """
+        Tries to create a raw disk via REST API without specifying 'sparse'
+        flag. Such call should fail.
+        """
+        master_domain = storagedomains.findMasterStorageDomain(
+            True, config.DATA_CENTER_NAME)[1]['masterDomain']
+        disk_name = "disk_%s" % self.tcms_test_case
+
+        assert disks.addDisk(
+            False, alias=disk_name, shareable=False, bootable=False,
+            size=1 * GB, storagedomain=master_domain, sparse=None,
+            format=ENUMS['format_raw'], interface=ENUMS['interface_ide'])
