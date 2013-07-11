4fdcbc3a4b9977ea8c190398df8e2739fd143428
Larisa Ustalov <lustalov@redhat.com>
2013-07-11 17:15:50 +0300

    ticket #2006: remove_packages plugin
    - remove_vdsm plugin generalized to remove any package
    - check if package exists before removal
    
    Change-Id: Ia83d2aeb3172a6f9840aafe3e17ec93eb2a2fc1f
    Reviewed-on: https://gerrit.eng.lab.tlv.redhat.com/8884
    Tested-by: Larisa Ustalov <lustalov@redhat.com>
    Reviewed-by: Elena Dolinin <edolinin@redhat.com>

--- art/test_handler/plmanagement/plugins/remove_packages_plugin.py
+++ art/test_handler/plmanagement/plugins/remove_packages_plugin.py
@@ -1,20 +1,20 @@
 """
 --------------------
-Remove vdsm packages Plugin
+Remove packages Plugin
 --------------------
 
-This plugin removes vdsm, vdsm-cli and vdsm-python
-packages from VDS machines defined in configuration file.
+This plugin removes packages installed on hosts.
+Names of packages should be defined in the configuration file.
 
 CLI Options:
 ------------
-    --remove-vdsm   Enable the plugin and remove all vdsm related packages
+    --remove-packages   Enable the plugin and remove all the defined packages
 
 Configuration Options:
 ----------------------
     | **[REMOVE_PACKAGES]**
     | **enabled** - to enable the plugin (true/false)
-    | **packages** - packages names, default: vdsm, vdsm-cli, vdsm-python
+    | **packages** - packages names
 """
 
 import logging
@@ -24,7 +24,7 @@ from art.test_handler.plmanagement.interfaces.application import IConfigurable
 from art.test_handler.plmanagement.interfaces.packaging import IPackaging
 from utilities.machine import Machine, LINUX
 
-logger = logging.getLogger(root_logger.name+'.remove_vdsm')
+logger = logging.getLogger('%s.remove_packages' % root_logger.name)
 
 PARAMETERS = 'PARAMETERS'
 PKG_SECTION = 'REMOVE_PACKAGES'
@@ -36,29 +36,29 @@ DEFAULT_PACKAGES = ['vdsm', 'vdsm-cli', 'vdsm-python', 'vdsm-xmlrpc']
 DEFAULT_STATE = False
 
 
-class VdsmRemoval(Component):
+class PackagesRemoval(Component):
     """
-    Removes vdsm packages from hosts.
+    Removes packages from hosts.
     """
     implements(IConfigurable, IPackaging)
 
-    name = "Remove vdsm"
+    name = "Remove packages"
     priority = 1000
 
     def __init__(self):
-        super(VdsmRemoval, self).__init__()
+        super(PackagesRemoval, self).__init__()
 
     @classmethod
     def add_options(cls, parser):
         group = parser.add_argument_group(cls.name, description=cls.__doc__)
-        group.add_argument('--remove-vdsm', action="store_true",
-                           dest='remove_vdsm', help="remove vdsm packages",
+        group.add_argument('--remove-packages', action="store_true",
+                           dest='remove_packages', help="remove packages",
                            default=False)
 
     def configure(self, params, conf):
         if not self.is_enabled(params, conf):
             return
-        logger.info("Configuring remove-vdsm plugin.")
+        logger.info("Configuring remove-packages plugin.")
 
         vds = conf[PARAMETERS].as_list(VDS)
         vds_passwd = conf[PARAMETERS].as_list(VDS_PASSWORD)
@@ -66,30 +66,33 @@ class VdsmRemoval(Component):
         packages = conf[PKG_SECTION].as_list(PACKAGES) if PKG_SECTION in conf \
             else DEFAULT_PACKAGES
 
-        logger.info('Removing vdsm packages...')
+        logger.info('Removing packages...')
         for name, passwd in zip(vds, vds_passwd):
             m = Machine(name, user, passwd).util(LINUX)
-            if not m.removeRpm(packages, True):
-                logger.error('Failed to remove vdsm packages from %s' % name)
+            for pkg in packages:
+                if m.checkRpm(pkg):
+                    if not m.removeRpm(pkg, True):
+                        logger.error('Failed to remove package %s from %s',
+                                     pkg, name)
 
     @classmethod
     def is_enabled(cls, params, conf):
         conf_en = conf[PKG_SECTION].as_bool(ENABLED) \
             if PKG_SECTION in conf else None
-        return params.remove_vdsm or conf_en
+        return params.remove_packages or conf_en
 
     @classmethod
     def fill_setup_params(cls, params):
-        params['name'] = 'remove_vdsm'
+        params['name'] = cls.name.lower().replace(' ', '-')
         params['version'] = '1.0'
         params['author'] = 'Larisa Ustalov'
         params['author_email'] = 'lustalov@redhat.com'
-        params['description'] = 'vdsm cleanup for ART'
+        params['description'] = 'packages cleanup for ART'
         params['long_description'] = 'Plugin for ART which is responsible '\
-               'for vdsm packages cleanup.'
+               'for packages cleanup.'
         params['requires'] = ['art-utilities']
         params['py_modules'] = \
-              ['art.test_handler.plmanagement.plugins.remove_vdsm_plugin']
+              ['art.test_handler.plmanagement.plugins.remove_packages_plugin']
 
     def config_spec(self, spec, val_funcs):
         section_spec = spec.get(PKG_SECTION, {})
