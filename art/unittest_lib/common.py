17c5d361dfb95b3876887989c6b548fafa61546b
Jakub Libosvar <jlibosva@redhat.com>
2013-07-24 13:18:35 +0200

    ART: Get BZ# status function
    Change-Id: Ic79d2e43c5069ffcccba5cb7b1cffd95f4a696b2
    Reviewed-on: https://gerrit.eng.lab.tlv.redhat.com/9029
    Tested-by: Jakub Libosvar <jlibosva@redhat.com>
    Reviewed-by: Elena Dolinin <edolinin@redhat.com>

---
+++ art/unittest_lib/common.py
@@ -0,0 +1,26 @@
+from art.test_handler.settings import plmanager
+
+try:
+    BZ_PLUGIN = [pl for pl in plmanager.configurables
+                 if pl.name == "Bugzilla"][0]
+except IndexError:
+    class FakeBZPlugin(object):
+        def __init__(self):
+            self.const_list = None
+
+        def is_state(self, *args):
+            """
+            Set all BZs as solved if BZ plugin is not available
+            """
+            return True
+    BZ_PLUGIN = FakeBZPlugin()
+
+
+def is_bz_state(bz_id):
+    """
+    Description: Decides if bz given by bz_id is in one of states given by
+    const_list (CLOSED, VERIFIED) by default or taken from config file
+    Parameters:
+        * bz_id - BZ number
+    """
+    return BZ_PLUGIN.is_state(bz_id, BZ_PLUGIN.const_list)
