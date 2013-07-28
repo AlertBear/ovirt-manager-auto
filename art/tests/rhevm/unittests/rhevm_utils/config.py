399d8b66f766bba253e57c945d31d69b31311dab
Michal Ovciarik <movciari@redhat.com>
2013-07-28 10:33:11 +0200

    rhevm_utils: Add more test cases for rhevm-config tool test
    Change-Id: I7115277c4bb0af92702b6572a1e49fbd84210ce0
    Reviewed-on: https://gerrit.eng.lab.tlv.redhat.com/9071
    Tested-by: Michal Ovciarik <movciari@redhat.com>
    Reviewed-by: Tareq Alayan <talayan@redhat.com>
    Reviewed-by: Elena Dolinin <edolinin@redhat.com>

--- art/tests/rhevm/unittests/rhevm_utils/config.py
+++ art/tests/rhevm/unittests/rhevm_utils/config.py
@@ -33,3 +33,88 @@ class ConfigTestCase(RHEVMUtilsTestCase):
             failed = True
         if failed:
             raise ex
+
+    @istest
+    @tcms(CONFIG_TEST_PLAN, 86797)
+    def configGet(self):
+        """
+        rhevm-config --get
+        """
+        failed = False
+        exceptions = []
+
+        self.ut(get='SSLEnabled')
+        try:
+            self.ut.autoTest()
+        except (errors.MissingPropertyFile, errors.OptionIsNotAllowed,
+            errors.FetchOptionError, errors.OutputVerificationError) as ex:
+                failed = True
+                exceptions.append(ex)
+
+        self.ut(get='SomeWeirdProperty', rc=1) # should return 1 when trying to get missing property
+        try:
+            self.ut.autoTest()
+        except (errors.MissingPropertyFile, errors.OptionIsNotAllowed,
+                errors.FetchOptionError, errors.OutputVerificationError) as ex:
+                failed = True
+                exceptions.append(ex)
+        self.ut.autoTest()
+
+        self.ut(get='LocalAdminPassword')
+        self.ut.autoTest()
+        try:
+            self.ut.autoTest()
+        except (errors.MissingPropertyFile, errors.OptionIsNotAllowed,
+                errors.FetchOptionError, errors.OutputVerificationError) as ex:
+            failed = True
+            exceptions.append(ex)
+
+        if failed:
+            raise exceptions
+
+    @istest
+    def configSet(self):
+        """
+        rhevm-config --set
+        """
+        failed = False
+        self.ut(set='SSLEnabled=false')
+        # set it to known value
+        current = self.ut.getValue('SSLEnabled')
+        try:
+            self.ut.autoTest()
+        except (ValueError, errors.OutputVerificationError,
+                errors.FailedToSetValue) as ex:
+            failed = True
+        assert current == 'false'
+
+        self.ut(set='SSLEnabled=true')
+        # change value
+        current = self.ut.getValue('SSLEnabled')
+        try:
+            self.ut.autoTest()
+        except (ValueError, errors.OutputVerificationError,
+                errors.FailedToSetValue) as ex:
+            failed = True
+        assert current == 'true'
+
+        self.ut(set='SSLEnabled=true')
+        # try if it works when new value is same as old
+        current = self.ut.getValue('SSLEnabled')
+        try:
+            self.ut.autoTest()
+        except (ValueError, errors.OutputVerificationError,
+                errors.FailedToSetValue) as ex:
+            failed = True
+        assert current == 'true'
+
+        if failed:
+            raise ex
+
+    @istest
+    def configAll(self):
+        """
+        rhevm-config --all
+        """
+        self.ut(all=None)
+        self.ut.autoTest()
