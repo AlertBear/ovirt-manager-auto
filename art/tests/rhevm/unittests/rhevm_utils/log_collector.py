b0ce42d90bacaf60950bad39208fdda3c3d144e5
Ilanit Stein <istein@redhat.com>
2013-08-21 17:13:42 +0300

    log collector test: Add tcms link to test cases
    Change-Id: I869efaedfe49a633affd9525f3ab38d23910f8a6
    Reviewed-on: https://gerrit.eng.lab.tlv.redhat.com/9447
    Tested-by: Ilanit Stein <istein@redhat.com>
    Reviewed-by: Tareq Alayan <talayan@redhat.com>

--- art/tests/rhevm/unittests/rhevm_utils/log_collector.py
+++ art/tests/rhevm/unittests/rhevm_utils/log_collector.py
@@ -1,7 +1,8 @@
 from rhevm_utils.base import RHEVMUtilsTestCase, istest, logger, \
     REST_API_PASS, LOG_COL_CONF
 from utilities.rhevm_tools.log_collector import  LogCollectorUtility
-
+from art.test_handler.tools import tcms
+LOG_COLLECTOR_TEST_PLAN = 3748
 NAME = 'log_collector'
 
 
@@ -13,6 +14,7 @@ class LogCollectorTestCase(RHEVMUtilsTestCase):
     _multiprocess_can_split_ = True
 
     @istest
+    @tcms(LOG_COLLECTOR_TEST_PLAN, 275493)
     def logCollectorList(self):
         """ log_collector list"""
         print ("debug: Inside logCollectorList function")
@@ -21,6 +23,7 @@ class LogCollectorTestCase(RHEVMUtilsTestCase):
         self.ut.autoTest()
 
     @istest
+    @tcms(LOG_COLLECTOR_TEST_PLAN, 95162)
     def logCollectorCollect(self):
         """ log_collector collect"""
         assert self.ut.setRestConnPassword(NAME, LOG_COL_CONF, REST_API_PASS)
