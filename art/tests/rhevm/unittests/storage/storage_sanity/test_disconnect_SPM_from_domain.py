c7fee11444cc4c1ba3937f1a5e7e2245a8b43bb3
ratamir <ratamir@redhat.com>
2013-10-27 15:50:45 +0200

    Storage Sanity fix
    Split disconnect_spm_from_pool test to new file to fix
    Storage Sanity test due to test failure on localfs
    
    Change-Id: Ifee2e520e80bcd4d6406fcbd01b34a322b01451a
    Reviewed-on: https://gerrit.eng.lab.tlv.redhat.com/10148
    Reviewed-by: Gadi Ickowicz <gickowic@redhat.com>
    Reviewed-by: Carlos Mestre Gonzalez <cmestreg@redhat.com>
    Tested-by: Gadi Ickowicz <gickowic@redhat.com>
    Reviewed-by: Meital Bourvine <mbourvin@redhat.com>

---
+++ art/tests/rhevm/unittests/storage/storage_sanity/test_disconnect_SPM_from_domain.py
@@ -0,0 +1,106 @@
+import logging
+from unittest import TestCase
+from nose.tools import istest
+from art.rhevm_api.tests_lib.high_level import datacenters
+from art.rhevm_api.tests_lib.low_level import storagedomains as ll_st_domains
+from art.rhevm_api.tests_lib.low_level.hosts import waitForSPM
+from art.test_handler.tools import tcms, bz
+from art.rhevm_api.tests_lib.low_level import hosts
+import art.rhevm_api.utils.storage_api as st_api
+import config
+
+logger = logging.getLogger(__name__)
+
+
+def setup_module():
+    """ creates datacenter, adds hosts, clusters, storages according to
+        config file
+    """
+    datacenters.build_setup(
+        config=config.PARAMETERS,
+        storage=config.PARAMETERS,
+        storage_type=config.DATA_CENTER_TYPE,
+        basename=config.BASENAME)
+
+
+def teardown_module():
+    """ removes created datacenter, storages etc.
+    """
+    ll_st_domains.cleanDataCenter(
+        True, config.DATA_CENTER_NAME, vdc=config.VDC,
+        vdc_password=config.VDC_PASSWORD)
+
+
+class TestCase68536(TestCase):
+    """
+    storage sanity test, disconnect SPM from storage
+    https://tcms.engineering.redhat.com/case/68536/
+    """
+    __test__ = True
+    tcms_plan_id = '2347'
+    tcms_test_case = '68536'
+    master_domain_ip = None
+    spm = None
+
+    @classmethod
+    def setup_class(cls):
+        logger.info("DC name : %s", config.DATA_CENTER_NAME)
+        cls.spm = hosts.getSPMHost(config.HOSTS)
+        logger.info("SPM found : %s",  cls.spm)
+
+        found, master_domain = ll_st_domains.findMasterStorageDomain(
+            True, config.DATA_CENTER_NAME)
+        master_domain = master_domain['masterDomain']
+        logger.info("Master domain found : %s", master_domain)
+
+        found, cls.master_domain_ip = ll_st_domains.getDomainAddress(
+            True, master_domain)
+        cls.master_domain_ip = cls.master_domain_ip['address']
+        logger.info("Master domain ip found : %s", cls.master_domain_ip)
+
+    @istest
+    @tcms(tcms_plan_id, tcms_test_case)
+    @bz('1017207')
+    def disconnect_SPM_from_storage_test(self):
+        """ test checks if disconnecting SPM from storage domain
+            works properly
+        """
+
+        logger.info("Blocking connectivity from SPM : %s to master domain : "
+                    "%s", self.spm, self.master_domain_ip)
+        found = st_api.blockOutgoingConnection(self.spm, 'root',
+                                               config.VDS_PASSWORD[0],
+                                               self.master_domain_ip)
+        self.assertTrue(found, "block connectivity to master domain failed")
+
+        logger.info("wait for state : 'Non Operational'")
+        state = hosts.waitForHostsStates(True, self.spm,
+                                         states=config.HOST_NONOPERATIONAL)
+        logger.info("state: %s", state)
+        self.assertTrue(state, "Cannot move to Non Operational state")
+
+        logger.info("wait for state : 'Non Responsive'")
+        state_is_fenced = \
+            hosts.waitForHostsStates(True, self.spm,
+                                     states=config.HOST_NONRESPONSIVE)
+
+        logger.info("state: %s", state_is_fenced)
+        self.assertFalse(state_is_fenced, "host fenced")
+
+        self.assertTrue(waitForSPM(config.DATA_CENTER_NAME, 120, 10),
+                        "wait for SPM")
+
+        new_spm = hosts.getSPMHost(config.HOSTS)
+        logger.info('new spm is %s', new_spm)
+        self.assertTrue(self.spm != new_spm, "New SPM isn't elected")
+
+    @classmethod
+    def teardown_class(cls):
+        """
+        unblocking the connection from SPM to master domain
+        """
+        logger.info("unblocking connectivity from SPM : %s to master domain : "
+                    "%s", cls.spm, cls.master_domain_ip)
+        st_api.unblockOutgoingConnection(cls.spm, 'root',
+                                         config.VDS_PASSWORD[0],
+                                         cls.master_domain_ip)
