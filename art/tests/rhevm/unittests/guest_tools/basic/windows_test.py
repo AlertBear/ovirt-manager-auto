3c0124606e6c1ca44c52409c15ea5a7e0f302392
Ondra Machacek <omachace@redhat.com>
2014-01-08 17:05:08 +0100

    guest_tools_tests: fix of guest tools tests
    * disabled windows XP 32/64b tests and win7 32b tests
    * set STAFCONVDIR var as workaround before RHEVM-863 will be fixed
    
    Change-Id: I1d3cd6f202ffca13191310358090be73dad5fa25
    Reviewed-on: https://gerrit.eng.lab.tlv.redhat.com/10766
    Reviewed-by: Katarzyna Jachim <kjachim@redhat.com>
    Tested-by: Ondra Machacek <omachace@redhat.com>
    Reviewed-by: Tareq Alayan <talayan@redhat.com>
    Reviewed-by: Lukas Bednar <lbednar@redhat.com>

--- art/tests/rhevm/unittests/guest_tools/basic/windows_test.py
+++ art/tests/rhevm/unittests/guest_tools/basic/windows_test.py
@@ -2,7 +2,6 @@
 Base class for all Windows basic sanity tests
 """
 import json
-from concurrent.futures import ThreadPoolExecutor
 import logging
 from nose.tools import istest
 from unittest import TestCase
@@ -10,7 +9,6 @@ from unittest2 import skipIf
 import os
 
 import art.rhevm_api.utils.test_utils as utils
-import art.test_handler.exceptions as exceptions
 import config
 import art.rhevm_api.tests_lib.low_level.vms as vms
 import art.rhevm_api.tests_lib.low_level.templates as template
@@ -18,14 +16,15 @@ import art.rhevm_api.tests_lib.low_level.templates as template
 LOGGER = logging.getLogger(__name__)
 SKIP_INSTALL = False
 SKIP_UNINSTALL = False
+STAFCONVDIR = 'STAFCONVDIR'
+STAF_PATH = '/usr/local/staf/codepage'
+
 
 def setup_module():
-    "setup module"
+    """ setup variables """
+    global SKIP_INSTALL, SKIP_UNINSTALL
     SKIP_INSTALL = bool(config.SKIP_INSTALL)
     SKIP_UNINSTALL = bool(config.SKIP_UNINSTALL)
-
-def teardown_module():
-    "teardown module"
 
 
 class Windows(TestCase):
@@ -47,17 +46,16 @@ class Windows(TestCase):
         "setup class"
         LOGGER.info("Setting up class %s",
                     cls.__name__)
-        template.importTemplate(True, template=cls.templateNameStorage,
-                        export_storagedomain=config.EXPORT_STORAGE_DOMAIN,
-                        import_storagedomain=config.STORAGE_DOMAIN,
-                        cluster=config.CLUSTER_NAME,
-                        name=cls.template)
+        template.importTemplate(
+            True, template=cls.templateNameStorage,
+            export_storagedomain=config.EXPORT_STORAGE_DOMAIN,
+            import_storagedomain=config.STORAGE_DOMAIN,
+            cluster=config.CLUSTER_NAME, name=cls.template)
         vms.createVm(True, vmName=cls.vmName,
                      vmDescription="VM for %s class" % cls.__name__,
                      cluster=config.CLUSTER_NAME,
                      template=cls.template)
-        vms.runVmOnce(
-            True, cls.vmName, cdrom_image=config.CD_WITH_TOOLS)
+        vms.runVmOnce(True, cls.vmName)
         vms.waitForVMState(vm=cls.vmName, state='up')
         cls.mac = vms.getVmMacAddress(
             True, vm=cls.vmName,
@@ -65,7 +63,7 @@ class Windows(TestCase):
         LOGGER.info("Mac adress is %s", cls.mac)
         cls.ip = utils.convertMacToIpAddress(
             True, cls.mac, subnetClassB=config.SUBNET_CLASS)[1].get('ip', None)
-        # os.environ['STAFCONVDIR'] = '/usr/local/staf/codepage'
+        os.environ[STAFCONVDIR] = STAF_PATH
         cls.machine = utils.createMachine(
             True, host=config.VDS, ip=cls.ip, os='windows', platf=cls.platf)
 
@@ -79,7 +77,7 @@ class Windows(TestCase):
     @skipIf(SKIP_INSTALL, "skiping installation")
     def installationUsingAPT(self):
         """
-        This tests function that returns parameters a
+        This function test installation of GT using APT
         """
         if not utils.isGtMachineReady(True, self.machine):
             return False
@@ -99,7 +97,7 @@ class Windows(TestCase):
     @skipIf(SKIP_UNINSTALL, "skiping uninstallation")
     def unistallGuestTools(self):
         """
-        This tests function that returns parameters a
+        This tests uninstallation of GT
         """
         if not utils.isGtMachineReady(True, self.machine):
             return False
@@ -112,7 +110,7 @@ class Windows(TestCase):
 
 class Windows7_64bit(Windows):
     """
-    This is positive test case with several actions
+    Setup for Windows 7 64 bit
     """
     __test__ = True
     vmName = config.WIN7_VM_NAME
@@ -121,22 +119,24 @@ class Windows7_64bit(Windows):
     platf = '64'
     toolsDict = config.WIN7_TOOLS_DICT
 
+
 class WindowsXP(Windows):
     """
-    This is positive test case with several actions
+    Setup for Windows 7 XP
     """
-    __test__ = True
+    __test__ = False
     templateNameStorage = config.WINXP_TEMPLATE_NAME
     vmName = config.WINXP_VM_NAME
     template = config.WINXP_IMPORTED_TEMPLATE_NAME
     platf = '32'
     toolsDict = config.WINXP_TOOLS_DICT
 
+
 class Windows7_32(Windows):
     """
-    This is positive test case with several actions
+    Setup for Windows 7 32 bit
     """
-    __test__ = True
+    __test__ = False
     templateNameStorage = config.WIN7_32_TEMPLATE_NAME
     vmName = config.WIN7_32_VM_NAME
     template = config.WIN7_32_IMPORTED_TEMPLATE_NAME
