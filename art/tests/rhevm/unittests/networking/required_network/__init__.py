43582fbca83b58c541fc40fe6d88ba501df1da7b
Meni Yakove <myakove@redhat.com>
2014-02-13 10:07:05 +0200

    Convert required_network XML to unittest
    Change-Id: Ied60ffafdedd91044d6ef4ab1e2ed5136b0348f6
    Reviewed-on: https://gerrit.eng.lab.tlv.redhat.com/11135
    Reviewed-by: Katarzyna Jachim <kjachim@redhat.com>
    Tested-by: Genadi Chereshnya <gcheresh@redhat.com>
    Reviewed-by: Genadi Chereshnya <gcheresh@redhat.com>
    Reviewed-by: Lukas Bednar <lbednar@redhat.com>

---
+++ art/tests/rhevm/unittests/networking/required_network/__init__.py
@@ -0,0 +1,43 @@
+"""
+RequiredNetwork Test
+"""
+
+import logging
+from art.rhevm_api.tests_lib.low_level.storagedomains import cleanDataCenter
+from art.rhevm_api.tests_lib.high_level.networks import prepareSetup
+from art.test_handler.exceptions import NetworkException
+
+logger = logging.getLogger("RequiredNetwork")
+
+#################################################
+
+
+def setup_package():
+    """
+    Prepare environment
+    """
+    import config
+    logger.info("Creating data center, cluster, adding host and storage")
+    if not prepareSetup(hosts=config.HOSTS[0],
+                        cpuName=config.CPU_NAME,
+                        username=config.HOSTS_USER,
+                        password=config.HOSTS_PW,
+                        datacenter=config.DC_NAME,
+                        storageDomainName=config.STORAGE_NAME,
+                        storage_type=config.STORAGE_TYPE,
+                        cluster=config.CLUSTER_NAME,
+                        lun_address=config.LUN_ADDRESS,
+                        lun_target=config.LUN_TARGET,
+                        luns=config.LUN, version=config.VERSION,
+                        auto_nics=[config.HOST_NICS[0]]):
+        raise NetworkException("Cannot create setup")
+
+
+def teardown_package():
+    """
+    Cleans the environment
+    """
+    import config
+    logger.info("Clean the environment")
+    if not cleanDataCenter(positive=True, datacenter=config.DC_NAME):
+        raise NetworkException("Cannot remove setup")
