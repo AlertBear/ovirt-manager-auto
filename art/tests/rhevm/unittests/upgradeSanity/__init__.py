44524e6e44f8e612bc483469d2569790db43bcb9
Tareq Alayan <talayan@redhat.com>
2014-03-23 19:32:51 +0200

    Upgrade do nothing test
    Due to the discrepancy of creating data center in 3.3 and 3.4.
    Currently This will allow CI to run only upgrade flow with no actions done
    in the engine.
    Different commit will contain tests.
    
    Change-Id: I078240cadb63698fcf5d7ff62d3d03272cd3e1aa
    Reviewed-on: https://gerrit.eng.lab.tlv.redhat.com/12104
    Tested-by: Tareq Alayan <talayan@redhat.com>
    Tested-by: Ohad Basan <obasan@redhat.com>
    Reviewed-by: Lukas Bednar <lbednar@redhat.com>

--- art/tests/rhevm/unittests/upgradeSanity/__init__.py
+++ art/tests/rhevm/unittests/upgradeSanity/__init__.py
@@ -1,24 +1,21 @@
-from art.rhevm_api.tests_lib.low_level import storagedomains as ll_st
-from art.rhevm_api.tests_lib.low_level.storagedomains import createDatacenter
-from art.rhevm_api.tests_lib.low_level import general
+import logging
+
+logger = logging.getLogger("upgradeSanity")
 
 
 def setup_module():
-
+    """
+    Prepare environment
+    """
     import config as cfg
     if not cfg.installed_matches_current_version():
-        createDatacenter(True, hosts=cfg.HOSTS[0], cpuName=cfg.CPU_NAME,
-                         username=cfg.HOSTS_USER, password=cfg.HOSTS_PW,
-                         datacenter=cfg.DC_NAME, storage_type=cfg.STORAGE_TYPE,
-                         cluster=cfg.CLUSTER_NAME, version=cfg.VERSION,
-                         dataStorageDomains=cfg.DATA_PATHS[0],
-                         address=cfg.DATA_ADDRESSES[0],
-                         sdNameSuffix=cfg.SD_SUFFIX)
+        logger.info("SETUP in source version")
 
 
 def teardown_module():
-
+    """
+    Cleans the environment
+    """
     import config as cfg
     if cfg.installed_matches_current_version():
-        ll_st.cleanDataCenter(True, cfg.DC_NAME, vdc=cfg.VDC,
-                              vdc_password=cfg.VDC_PASSWORD)
+        logger.info("TEARDOWN in destination version")
--- art/tests/rhevm/unittests/upgradeSanity/test_upgradeSanity.py
+++ art/tests/rhevm/unittests/upgradeSanity/test_upgradeSanity.py
@@ -1,7 +1,5 @@
 '''
 Sanity testing of upgrade.
-1 DC, 1 Cluster, 1 host, 1 SD and 1 VM will be created.
-Test will create and run VM on 3.2 setup then upgrade to 3.3 and try the same.
 '''
 
 import config as cfg
@@ -14,22 +12,16 @@ LOGGER = logging.getLogger(__name__)
 
 
 class UpgradeSanityTest(TestCase):
-    """ Basic run vm test """
+    """ Basic test """
     __test__ = True
 
     def setUp(self):
         LOGGER.debug("class UpgradeSanityTest setUp")
-        assert vms.createVm(True, cfg.VM_NAME, '',
-                            cluster=cfg.CLUSTER_NAME,
-                            storageDomainName=cfg.STORAGE_NAME, size=cfg.GB,
-                            network=config.MGMT_BRIDGE)
 
     def tearDown(self):
         LOGGER.debug("class UpgradeSanityTest tearDown")
-        assert vms.removeVm(True, cfg.VM_NAME, stopVM='true', wait=True)
 
     @istest
-    def run_vm(self):
-        """ Run vm """
-        LOGGER.debug("class UpgradeSanityTest run_vm")
-        self.assertTrue(vms.startVm(True, cfg.VM_NAME))
+    def run_test(self):
+        """ Test Case placeholder """
+        LOGGER.debug("class UpgradeSanityTest test case")
