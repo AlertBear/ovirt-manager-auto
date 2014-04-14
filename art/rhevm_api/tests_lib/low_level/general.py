80f37c998c901384ebaad0806c0e9ce53de6ce98
Katarzyna Jachim <kjachim@redhat.com>
2014-04-14 13:19:13 +0200

    Fixed pep8 errors in low_level.general
    Change-Id: I43ec2cc09833b023220ecf3067fdfd283d7e9937
    Reviewed-on: https://gerrit.eng.lab.tlv.redhat.com/12355
    Tested-by: Katarzyna Jachim <kjachim@redhat.com>
    Reviewed-by: Petr Benas <pbenas@redhat.com>
    Reviewed-by: Katarzyna Jachim <kjachim@redhat.com>

--- art/rhevm_api/tests_lib/low_level/general.py
+++ art/rhevm_api/tests_lib/low_level/general.py
@@ -46,7 +46,8 @@ def getSystemVersion():
     Author: jlibosva
     Return: Tuple containing version (major, minor)
     '''
-    system_version = util.get(href='', absLink=False).get_product_info().get_version()
+    system_version = util.get(
+        href='', absLink=False).get_product_info().get_version()
     system_major = system_version.get_major()
     system_minor = system_version.get_minor()
 
@@ -139,85 +140,89 @@ def convToInt(num, attr):
 
 @is_action()
 def checkSummary(positive, domain):
-     '''
-     Description: validate system summary statistics values
-     Author: edolinin
-     Parameters:
-       no
-     Return: status (True if all statistics values are correct, False otherwise)
-     '''
+    '''
+    Description: validate system summary statistics values
+    Author: edolinin
+    Parameters:
+      no
+    Return: status (True if all statistics values are correct, False otherwise)
+    '''
 
-     getAll = util.get(href='', absLink=False)
-     status = True
+    getAll = util.get(href='', absLink=False)
+    status = True
 
-     vms = vmUtil.get(absLink=False)
-     sumVmsTotal = getAll.get_summary().get_vms().total
-     util.logger.info('Comparing total vms number')
-     if not validator.compareCollectionSize(vms, sumVmsTotal, util.logger):
+    vms = vmUtil.get(absLink=False)
+    sumVmsTotal = getAll.get_summary().get_vms().total
+    util.logger.info('Comparing total vms number')
+    if not validator.compareCollectionSize(vms, sumVmsTotal, util.logger):
         status = False
 
-     vms = vmUtil.get(absLink=False)
-     sumVmsAct = getAll.get_summary().get_vms().active
-     vms = filter(lambda x: x.status.state == 'up', vms)
-     util.logger.info('Comparing active vms number')
-     if not validator.compareCollectionSize(vms, sumVmsAct, util.logger):
+    vms = vmUtil.get(absLink=False)
+    sumVmsAct = getAll.get_summary().get_vms().active
+    vms = filter(lambda x: x.status.state == 'up', vms)
+    util.logger.info('Comparing active vms number')
+    if not validator.compareCollectionSize(vms, sumVmsAct, util.logger):
         status = False
 
-     hosts= hostUtil.get(absLink=False)
-     sumHostsTotal = getAll.get_summary().get_hosts().total
-     util.logger.info('Comparing total hosts number')
-     if not validator.compareCollectionSize(hosts, sumHostsTotal, util.logger):
+    hosts = hostUtil.get(absLink=False)
+    sumHostsTotal = getAll.get_summary().get_hosts().total
+    util.logger.info('Comparing total hosts number')
+    if not validator.compareCollectionSize(hosts, sumHostsTotal, util.logger):
         status = False
 
-     hosts = hostUtil.get(absLink=False)
-     sumHostsAct = getAll.get_summary().get_hosts().active
-     hosts = filter(lambda x: x.get_status().get_state() == 'up', hosts)
-     util.logger.info('Comparing active hosts number')
-     if not validator.compareCollectionSize(hosts, sumHostsAct, util.logger):
+    hosts = hostUtil.get(absLink=False)
+    sumHostsAct = getAll.get_summary().get_hosts().active
+    hosts = filter(lambda x: x.get_status().get_state() == 'up', hosts)
+    util.logger.info('Comparing active hosts number')
+    if not validator.compareCollectionSize(hosts, sumHostsAct, util.logger):
         status = False
 
-     users= userUtil.get(absLink=False)
-     sumUsersTotal = getAll.get_summary().get_users().total
-     util.logger.info('Comparing total users number')
-     if not validator.compareCollectionSize(users, sumUsersTotal, util.logger):
+    users = userUtil.get(absLink=False)
+    sumUsersTotal = getAll.get_summary().get_users().total
+    util.logger.info('Comparing total users number')
+    if not validator.compareCollectionSize(users, sumUsersTotal, util.logger):
         status = False
 
-     users_active = []
-     domainObj = domUtil.find(domain)
-     for user in users:
-        domainUser = util.getElemFromElemColl(domainObj, user.get_name(), 'users', 'user')
+    users_active = []
+    domainObj = domUtil.find(domain)
+    for user in users:
+        domainUser = util.getElemFromElemColl(
+            domainObj, user.get_name(), 'users', 'user')
         if domainUser:
             users_active.append(user)
 
-     util.logger.info('Comparing active users number')
-     sumUsersActive = getAll.get_summary().get_users().active
-     if not validator.compareCollectionSize(users_active, sumUsersActive, util.logger):
+    util.logger.info('Comparing active users number')
+    sumUsersActive = getAll.get_summary().get_users().active
+    if not validator.compareCollectionSize(
+            users_active, sumUsersActive, util.logger):
         status = False
 
-     util.logger.info('Comparing total storages number')
-     sumSDTotal = getAll.get_summary().get_storage_domains().total
-     storageDomains = sdUtil.get(absLink=False)
-     if not validator.compareCollectionSize(storageDomains, sumSDTotal, util.logger):
+    util.logger.info('Comparing total storages number')
+    sumSDTotal = getAll.get_summary().get_storage_domains().total
+    storageDomains = sdUtil.get(absLink=False)
+    if not validator.compareCollectionSize(
+            storageDomains, sumSDTotal, util.logger):
         status = False
 
-     util.logger.info('Comparing active storages number')
-     sumSDActive = getAll.get_summary().get_storage_domains().active
-     sdActive = []
-     dcs = dcUtil.get(absLink=False)
-     for dc in dcs:
-         dcStorages = util.getElemFromLink(dc, link_name='storagedomains',
-                                            attr='storage_domain', get_href=False)
-         for dcSd in dcStorages:
+    util.logger.info('Comparing active storages number')
+    sumSDActive = getAll.get_summary().get_storage_domains().active
+    sdActive = []
+    dcs = dcUtil.get(absLink=False)
+    for dc in dcs:
+        dcStorages = util.getElemFromLink(
+            dc, link_name='storagedomains', attr='storage_domain',
+            get_href=False)
+        for dcSd in dcStorages:
             try:
                 if dcSd.status.state == 'active':
                     sdActive.append(dcSd)
             except AttributeError:
                 pass
 
-     if not validator.compareCollectionSize(sdActive, sumSDActive, util.logger):
+    if not validator.compareCollectionSize(sdActive, sumSDActive, util.logger):
         status = False
 
-     return status
+    return status
 
 
 @is_action()
@@ -245,8 +250,9 @@ def checkResponsesAreXsdValid():
     try:
         for error in util.xsd_schema_errors:
             href, ret, ex = error
-            util.logger.error("Response href: %s, status %d is not valid against xsd schema"
-                    % (href, ret['status']))
+            util.logger.error(
+                "Response href: %s, status %d is not valid against xsd schema"
+                % (href, ret['status']))
             util.logger.error("Response body: %s" % ret['body'])
             util.logger.error("Exception is: %s" % ex)
             ret = False
@@ -264,7 +270,8 @@ def getProductName():
     Return True, dict(product name) is succeeded, Fals, dict('') otherwise
     '''
     try:
-        product_name = util.get(href='', absLink=False).get_product_info().get_name()
+        product_name = util.get(
+            href='', absLink=False).get_product_info().get_name()
     except IndexError:
         return False, {'product_name': ''}
     return True, {'product_name': product_name}
@@ -284,8 +291,9 @@ def checkProductName(name):
     product_name = ret['product_name']
 
     if product_name != name:
-        util.logger.error("Product name is wrong, expected: '%s', actual: '%s'" \
-                    % (name, product_name))
+        util.logger.error(
+            "Product name is wrong, expected: '%s', actual: '%s'"
+            % (name, product_name))
         return False
 
     util.logger.info("Product name is correct: '%s'" % name)
