04ce9c254b1eadd0c83051c01c1513719b65778d
Meni Yakove <myakove@redhat.com>
2014-08-06 16:32:23 +0300

    IO: Clean init for golden_env
    Change-Id: Ib250b06d507ffb37fc290ad7efa3ca05738a37d0
    Reviewed-on: https://gerrit.eng.lab.tlv.redhat.com/13649
    Tested-by: Meni Yakove <myakove@redhat.com>
    Reviewed-by: Martin Pavlik <mpavlik@redhat.com>
    Reviewed-by: Katarzyna Jachim <kjachim@redhat.com>

--- art/tests/rhevmtests/networking/io_test/__init__.py
+++ art/tests/rhevmtests/networking/io_test/__init__.py
@@ -20,6 +20,10 @@ def setup_package():
     create 1 Cluster
     add 1 Host
     """
+    if config.GOLDEN_ENV:
+        logger.info("Running on golden env, no setup")
+        return
+
     logger.info("Create setup with datacenter, cluster and host")
     if not create_basic_setup(datacenter=config.DC_NAME[0],
                               storage_type=config.STORAGE_TYPE,
@@ -34,6 +38,10 @@ def teardown_package():
     """
     Cleans environment by removing Host, Cluster and DC from the setup
     """
+    if config.GOLDEN_ENV:
+        logger.info("Running on golden env, no teardown")
+        return
+
     if not removeHost(positive=True, host=config.HOSTS[0],
                       deactivate=True):
         raise NetworkException("Cannot remove host %s from Cluster %s" %
