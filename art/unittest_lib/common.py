e0d9e4657700df187b1a6457a53db0f19a223c52
Ilanit Stein <istein@redhat.com>
2014-08-13 16:40:49 +0300

    Rewrite bz decorator (by alukiano)
    Change-Id: I410c0cf8c3656f4c133d881643678ab12c266176
    Reviewed-on: https://gerrit.eng.lab.tlv.redhat.com/13747
    Reviewed-by: Artyom Lukianov <alukiano@redhat.com>
    Tested-by: Ilanit Stein <istein@redhat.com>
    Reviewed-by: Lukas Bednar <lbednar@redhat.com>

--- art/unittest_lib/common.py
+++ art/unittest_lib/common.py
@@ -1,6 +1,11 @@
+import logging
+from functools import wraps
 from art.test_handler.settings import plmanager, opts
+from art.test_handler.exceptions import SkipTest
 from unittest import TestCase
 from nose.plugins.attrib import attr
+
+logger = logging.getLogger(__name__)
 
 try:
     BZ_PLUGIN = [pl for pl in plmanager.configurables
@@ -16,6 +21,30 @@ except IndexError:
             """
             return True
     BZ_PLUGIN = FakeBZPlugin()
+
+
+def bz(bug_dict):
+    """
+    Decorator function to skip test case, when we have opened bug for it.
+
+    :param bug_dict: mapping bug to engine.
+    :type bug_dict: dictionary.
+    :raises: SkipTest
+    :returns: function object
+    """
+    def real_bz(func):
+        # noinspection PyUnusedLocal
+        @wraps(func)
+        def skip_if_bz(*args, **kwargs):
+            for bz_id, engine in bug_dict.iteritems():
+                engine_in = engine is None or opts['engine'] in engine
+                if not is_bz_state(bz_id) and engine_in:
+                    logger.warn("Skipping test because BZ%s for engine %s",
+                                bz_id, opts['engine'])
+                    raise SkipTest("BZ%s" % bz_id)
+            return func(*args, **kwargs)
+        return skip_if_bz
+    return real_bz
 
 
 def is_bz_state(bz_id):
