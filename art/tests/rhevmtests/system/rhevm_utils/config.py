daa31fb47052a7fe4c101ed45e6a516fa51b422f
Petr Benas <pbenas@redhat.com>
2014-08-18 09:57:15 +0200

    rhevm_config: fix pylint
    Change-Id: I04c8563ceaf298cceb0dd08c689537c5a08cf58c
    Reviewed-on: https://gerrit.eng.lab.tlv.redhat.com/13791
    Tested-by: Petr Benas <pbenas@redhat.com>
    Reviewed-by: Katarzyna Jachim <kjachim@redhat.com>
    Reviewed-by: Lukas Bednar <lbednar@redhat.com>

--- art/tests/rhevmtests/system/rhevm_utils/config.py
+++ art/tests/rhevmtests/system/rhevm_utils/config.py
@@ -1,6 +1,5 @@
 from rhevmtests.system.rhevm_utils import base
 from utilities.rhevm_tools.config import ConfigUtility
-from utilities.rhevm_tools import errors
 from art.test_handler.tools import tcms  # pylint: disable=E0611
 from art.unittest_lib import attr
 
@@ -21,97 +20,48 @@ class ConfigTestCase(base.RHEVMUtilsTestCase):
         """
         config list long option
         """
-        failed = False
         self.ut('--list')
-        try:
-            self.ut.autoTest()
-        except (errors.ConfigUtilityError,
-                errors.OutputVerificationError) as ex:
-            failed = True
+        self.ut.autoTest()
         self.ut('-l')
-        try:
-            self.ut.autoTest()
-        except (errors.ConfigUtilityError,
-                errors.OutputVerificationError) as ex:
-            failed = True
-        if failed:
-            raise ex
+        self.ut.autoTest()
 
     @tcms(CONFIG_TEST_PLAN, 86797)
     def test_config_get(self):
         """
         rhevm-config --get
         """
-        failed = False
-        exceptions = []
 
         self.ut(get='SSLEnabled')
-        try:
-            self.ut.autoTest()
-        except (errors.MissingPropertyFile, errors.OptionIsNotAllowed,
-                errors.FetchOptionError, errors.OutputVerificationError) as ex:
-                failed = True
-                exceptions.append(ex)
+        self.ut.autoTest()
 
         # should return 1 when trying to get missing property
         self.ut(get='SomeWeirdProperty', rc=1)
-        try:
-            self.ut.autoTest()
-        except (errors.MissingPropertyFile, errors.OptionIsNotAllowed,
-                errors.FetchOptionError, errors.OutputVerificationError) as ex:
-                failed = True
-                exceptions.append(ex)
         self.ut.autoTest()
 
         self.ut(get='LocalAdminPassword')
         self.ut.autoTest()
-        try:
-            self.ut.autoTest()
-        except (errors.MissingPropertyFile, errors.OptionIsNotAllowed,
-                errors.FetchOptionError, errors.OutputVerificationError) as ex:
-            failed = True
-            exceptions.append(ex)
-
-        if failed:
-            raise exceptions
 
     def test_config_set(self):
         """
         rhevm-config --set
         """
-        failed = False
         self.ut(set='SSLEnabled=false')
         # set it to known value
         current = self.ut.getValue('SSLEnabled')
-        try:
-            self.ut.autoTest()
-        except (ValueError, errors.OutputVerificationError,
-                errors.FailedToSetValue) as ex:
-            failed = True
-        assert current == 'false'
+        self.ut.autoTest()
+        self.assertEqual(current, 'false')
 
         self.ut(set='SSLEnabled=true')
         # change value
         current = self.ut.getValue('SSLEnabled')
-        try:
-            self.ut.autoTest()
-        except (ValueError, errors.OutputVerificationError,
-                errors.FailedToSetValue) as ex:
-            failed = True
-        assert current == 'true'
+        self.ut.autoTest()
+        self.assertEqual(current, 'true')
 
         self.ut(set='SSLEnabled=true')
         # try if it works when new value is same as old
         current = self.ut.getValue('SSLEnabled')
-        try:
-            self.ut.autoTest()
-        except (ValueError, errors.OutputVerificationError,
-                errors.FailedToSetValue) as ex:
-            failed = True
-        assert current == 'true'
-
-        if failed:
-            raise ex
+        self.ut.autoTest()
+        self.assertEqual(current, 'true')
 
     def test_config_all(self):
         """
