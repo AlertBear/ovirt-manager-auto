1eaf7c484205f4c9d6ee1ad57f2cf977a35fa250
Meni Yakove <myakove@redhat.com>
2014-11-16 12:03:51 +0200

    New job: BigRangeMacPool
    Change-Id: Ic5783a2020cd3434571aa6066fa40718bf34129b
    Reviewed-on: https://gerrit.eng.lab.tlv.redhat.com/14912
    Tested-by: Meni Yakove <myakove@redhat.com>
    Reviewed-by: Nelly Credi <ncredi@redhat.com>

---
+++ art/tests/rhevmtests/networking/big_ranges_mac_pool_manager/__init__.py
@@ -0,0 +1,66 @@
+"""
+Allow big ranges in MacPoolManager
+"""
+
+import logging
+from art.rhevm_api.tests_lib.high_level.networks import prepareSetup
+from art.rhevm_api.tests_lib.low_level.storagedomains import cleanDataCenter
+from art.rhevm_api.utils.test_utils import(
+    set_engine_properties, get_engine_properties
+)
+from art.test_handler.exceptions import NetworkException
+from rhevmtests.networking import config
+
+logger = logging.getLogger("BigRangeMacPool_init")
+
+ENGINE_DEFAULT_MAC_RANGE = []
+
+
+def setup_package():
+    """
+    Prepare environment
+    """
+    if config.GOLDEN_ENV:
+        logger.info("Running on golden env")
+        logger.info("Get engine default MAC pool range")
+        engine_default_mac_range = get_engine_properties(
+            config.ENGINE, [config.MAC_POOL_RANGE_CMD]
+        )[0]
+        ENGINE_DEFAULT_MAC_RANGE.append(engine_default_mac_range)
+
+    else:
+        logger.info("Create setup with datacenter, cluster, host and storage")
+        if not prepareSetup(
+            hosts=config.VDS_HOSTS[0], cpuName=config.CPU_NAME,
+            username=config.HOSTS_USER, password=config.HOSTS_PW,
+            datacenter=config.DC_NAME[0],
+            storageDomainName=config.STORAGE_NAME[0],
+            storage_type=config.STORAGE_TYPE, cluster=config.CLUSTER_NAME[0],
+            lun_address=config.LUN_ADDRESS[0], lun_target=config.LUN_TARGET[0],
+            luns=config.LUN[0], version=config.COMP_VERSION
+        ):
+            raise NetworkException("Cannot create setup")
+
+
+def teardown_package():
+    """
+    Cleans the environment
+    """
+    if config.GOLDEN_ENV:
+        logger.info("Running on golden env")
+        logger.info("Setting engine MacPoolRange to default")
+        cmd = "=".join(
+            [config.MAC_POOL_RANGE_CMD, ENGINE_DEFAULT_MAC_RANGE[0]]
+        )
+        if not set_engine_properties(config.ENGINE_HOST, [cmd]):
+            raise NetworkException(
+                "Failed to set MAC: %s", ENGINE_DEFAULT_MAC_RANGE[0]
+            )
+
+    else:
+        logger.info("Removing setup")
+        if not cleanDataCenter(
+                positive=True, datacenter=config.DC_NAME[0],
+                vdc=config.VDC_HOST, vdc_password=config.VDC_ROOT_PASSWORD
+        ):
+            raise NetworkException("Cannot remove setup")
