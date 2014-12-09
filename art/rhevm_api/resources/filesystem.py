ae2d5bcf0d4ed5e0179297cd5120b5584bfe3c37
Lukas Bednar <lbednar@redhat.com>
2014-12-09 16:26:40 +0100

    resources.filesystem: adding remove path methods
    Change-Id: Iaad6371d8f02a0efa7c3b058a642ef397340acfa
    Reviewed-on: https://gerrit.eng.lab.tlv.redhat.com/15120
    Reviewed-by: Lukas Bednar <lbednar@redhat.com>
    Tested-by: Lukas Bednar <lbednar@redhat.com>

--- art/rhevm_api/resources/filesystem.py
+++ art/rhevm_api/resources/filesystem.py
@@ -3,11 +3,13 @@ from art.rhevm_api.resources.service import Service
 
 class FileSystem(Service):
     """
-    class for working with filesystem
+    Class for working with filesystem.
+    It has same interface as 'os' module.
     """
     def _exec_file_test(self, op, path):
         return self.host.executor().run_cmd(
-            ['[', '-%s' % op, path, ']'])[0] == 0
+            ['[', '-%s' % op, path, ']']
+        )[0] == 0
 
     def exists(self, path):
         return self._exec_file_test('e', path)
@@ -17,3 +19,14 @@ class FileSystem(Service):
 
     def isdir(self, path):
         return self._exec_file_test('d', path)
+
+    def remove(self, path):
+        return self.host.executor().run_cmd(
+            ['rm', '-f', path]
+        )[0] == 0
+    unlink = remove
+
+    def rmdir(self, path):
+        return self.host.executor().run_cmd(
+            ['rm', '-rf', path]
+        )[0] == 0
