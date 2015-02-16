bf8314f994557ae93ad02f66567b2faf3d1e4872
glazarov <glazarov@redhat.com>
2015-02-16 15:43:14 +0200

    Fix for Storage sanity on code regressions
     Fix for failing cases on run http://jenkins.qa.lab.tlv.redhat.com:8080/view/Storage/view/3.5/job/3.5-storage_sanity_unittest-nfs/5/console
    
    Change-Id: I969985df4c732a928a2539c82524279b614f070d
    Reviewed-on: https://gerrit.eng.lab.tlv.redhat.com/15818
    Reviewed-by: Carlos Mestre Gonzalez <cmestreg@redhat.com>
    Reviewed-by: Raz Tamir <ratamir@redhat.com>
    Tested-by: Gilad Lazarovich <glazarov@redhat.com>
    Reviewed-by: Lukas Bednar <lbednar@redhat.com>

--- art/tests/rhevmtests/storage/storage_sanity/config.py
+++ art/tests/rhevmtests/storage/storage_sanity/config.py
@@ -7,9 +7,10 @@ from rhevmtests.storage.config import *  # flake8: noqa
 
 TESTNAME = "storage_sanity_unittest"
 
+EXTEND_LUN = None
+
 if not GOLDEN_ENV:
     if STORAGE_TYPE == STORAGE_TYPE_ISCSI:
-        EXTEND_LUN = STORAGE_CONF['PARAMETERS.extend_lun']
         EXTEND_LUN_ADDRESS = PARAMETERS.as_list("extend_lun_address")
         EXTEND_LUN_TARGET = PARAMETERS.as_list("extend_lun_target")
         EXTEND_LUN = PARAMETERS.as_list("extend_lun")
@@ -19,11 +20,12 @@ else:
         EXTEND_LUN_TARGET = UNUSED_LUN_TARGETS
         EXTEND_LUN = UNUSED_LUNS
 
-UNUSED_LUNS = {
-    "lun_targets": EXTEND_LUN_TARGET,
-    "lun_addresses": EXTEND_LUN_ADDRESS,
-    "lun_list": EXTEND_LUN
-}
+if EXTEND_LUN:
+    UNUSED_LUNS = {
+        "lun_addresses": EXTEND_LUN_ADDRESS,
+        "lun_targets": EXTEND_LUN_TARGET,
+        "lun_list": EXTEND_LUN
+    }
 
 HOST_NONOPERATIONAL = ENUMS["search_host_state_non_operational"]
 HOST_NONRESPONSIVE = ENUMS["search_host_state_non_responsive"]
