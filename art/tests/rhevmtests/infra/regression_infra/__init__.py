5a7283796d783bebbe1e96f75052973d2fec419b
Kobi Hakimi <khakimi@redhat.com>
2015-06-25 13:42:15 +0300

    Update Default mac pool object in regression mix
    update it with the mac_range parameter which allocated
    by mac broker and saved in PARAMETER.mac_range.
    
    Change-Id: Ie4aa24310e2d281410ddbe63892914c3b3e07b9f
    Reviewed-on: https://code.engineering.redhat.com/gerrit/51595
    Tested-by: Kobi Hakimi <khakimi@redhat.com>
    Reviewed-by: Nelly Credi <ncredi@redhat.com>
    Reviewed-by: Kobi Hakimi <khakimi@redhat.com>

--- art/tests/rhevmtests/infra/regression_infra/__init__.py
+++ art/tests/rhevmtests/infra/regression_infra/__init__.py
@@ -2,6 +2,7 @@ import os
 
 from art.rhevm_api.utils.aaa import copy_extension_file
 from art.rhevm_api.utils.test_utils import restart_engine
+from art.rhevm_api.tests_lib.high_level import mac_pool as hl_mac_pool
 
 from rhevmtests.infra.regression_infra import config
 
@@ -10,9 +11,7 @@ INSTALLED_FIXTURES = []
 
 
 def setup_package():
-    """
-    Install AAA extension properties
-    """
+    # Install AAA extension properties
     fixtures_dir = os.path.join(os.path.dirname(__file__), 'fixtures')
     for file_ in os.listdir(fixtures_dir):
         ext_file = os.path.join(fixtures_dir, file_)
@@ -21,12 +20,13 @@ def setup_package():
         INSTALLED_FIXTURES.append(target_file)
     restart_engine(config.ENGINE, 5, config.ENGINE_RESTART_TIMEOUT)
 
+    # Update the Default mac pool object with mac range from mac broker
+    hl_mac_pool.update_default_mac_pool()
+
 
 def teardown_package():
-    """
-    Wipe out AAA extension properties
-    """
-    # global is necesary here becasue of assignment in this scope
+    # Wipe out AAA extension properties
+    # the global is necessary here because of assignment in this scope
     global INSTALLED_FIXTURES
     if INSTALLED_FIXTURES:
         cmd = ['rm', '-f'] + INSTALLED_FIXTURES
