e0f6fb5358ef58528fb6a047905cd90df43698a6
Petr Balogh <pbalogh@redhat.com>
2015-07-20 13:30:17 +0200

    rhevm-art: check unfinished jobs in DB
    Check for unfinished jobs before and after tier jobs and if there is
    some, it changes its status to FINISHED before run tests, and on the
    end it only prints unfinished jobs and set build to UNSTABLE
    
    Related patch: https://code.engineering.redhat.com/gerrit/#/c/53337/
    Jira ticket: https://projects.engineering.redhat.com/browse/RHEVM-2150
    
    Change-Id: I9f235575f6f836306f365bbe9bf14e96e4f6d058
    Reviewed-on: https://code.engineering.redhat.com/gerrit/53336
    Tested-by: Petr Balogh <pbalogh@redhat.com>
    Reviewed-by: Kobi Hakimi <khakimi@redhat.com>
    Reviewed-by: Genadi Chereshnya <gcheresh@redhat.com>
    Reviewed-by: Raz Tamir <ratamir@redhat.com>
    Reviewed-by: Nelly Credi <ncredi@redhat.com>

--- art/tests/rhevmtests/__init__.py
+++ art/tests/rhevmtests/__init__.py
@@ -1,3 +1,18 @@
 """
 This package contains RHEVM tests
 """
+import logging
+import art.rhevm_api.tests_lib.low_level.jobs as ll_jobs
+import helpers
+
+logger = logging.getLogger(__name__)
+
+
+def setup_package():
+    """ Set unfinished jobs to FINISHED status before run tests """
+    helpers.clean_unfinished_jobs_on_engine(ll_jobs.get_active_jobs())
+
+
+def teardown_package():
+    """ Check unfinished jobs after all tests """
+    helpers.get_unfinished_jobs_list()
