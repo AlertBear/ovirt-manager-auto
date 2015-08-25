59fad93d49d14f4562bc65eb63191d2652af7a8f
obensass <obensass@redhat.com>
2015-08-25 16:03:01 +0300

    Network: run bridgeless only on GE
    Change-Id: Ica7ba92f1cf839a3975271fe7a345a51b7fa8b4f
    Reviewed-on: https://code.engineering.redhat.com/gerrit/56214
    Tested-by: Ori Ben Sasson <obensass@redhat.com>
    Reviewed-by: Meni Yakove <myakove@redhat.com>
    Reviewed-by: Kobi Hakimi <khakimi@redhat.com>
    Reviewed-by: Genadi Chereshnya <gcheresh@redhat.com>

--- art/tests/rhevmtests/networking/bridgeless/__init__.py
+++ art/tests/rhevmtests/networking/bridgeless/__init__.py
@@ -3,11 +3,7 @@ Bridgeless network feature test
 """
 
 import logging
-from rhevmtests.networking import config, network_cleanup
-from art.rhevm_api.tests_lib.high_level.networks import(
-    create_basic_setup, remove_basic_setup
-)
-from art.test_handler.exceptions import NetworkException
+import rhevmtests.networking as networking
 logger = logging.getLogger("Bridgeless_Networks_Init")
 
 #################################################
@@ -15,33 +11,6 @@ logger = logging.getLogger("Bridgeless_Networks_Init")
 
 def setup_package():
     """
-    Prepare environment
+    running cleanup
     """
-    if config.GOLDEN_ENV:
-        logger.info("Running on golden env, no setup")
-        network_cleanup()
-        return
-
-    logger.info("Create setup with datacenter, cluster and host")
-    if not create_basic_setup(datacenter=config.DC_NAME[0],
-                              storage_type=config.STORAGE_TYPE,
-                              version=config.COMP_VERSION,
-                              cluster=config.CLUSTER_NAME[0],
-                              cpu=config.CPU_NAME, host=config.HOSTS[0],
-                              host_password=config.HOSTS_PW):
-        raise NetworkException("Failed to create setup")
-
-
-def teardown_package():
-    """
-    Cleans the environment
-    """
-    if config.GOLDEN_ENV:
-        logger.info("Running on golden env, no teardown")
-        return
-
-    logger.info("Removing DC/Cluster and host")
-    if not remove_basic_setup(datacenter=config.DC_NAME[0],
-                              cluster=config.CLUSTER_NAME[0],
-                              hosts=config.HOSTS[0]):
-        logger.error("Failed to remove setup")
+    networking.network_cleanup()
