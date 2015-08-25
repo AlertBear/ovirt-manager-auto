1f94d42a083d90607e23ea4bafe93d138c25ad93
obensass <obensass@redhat.com>
2015-08-25 10:13:19 +0300

    Network:run arbitrary_vlan only on GE
    Change-Id: Icf44ff0d684f65dbdfe24038a6a1d2ea0ce0e857
    Reviewed-on: https://code.engineering.redhat.com/gerrit/56167
    Tested-by: Ori Ben Sasson <obensass@redhat.com>
    Reviewed-by: Meni Yakove <myakove@redhat.com>
    Reviewed-by: Kobi Hakimi <khakimi@redhat.com>

--- art/tests/rhevmtests/networking/arbitrary_vlan_device_name/__init__.py
+++ art/tests/rhevmtests/networking/arbitrary_vlan_device_name/__init__.py
@@ -12,31 +12,15 @@ import logging
 import rhevmtests.helpers as helpers
 import rhevmtests.networking as network
 import art.test_handler.exceptions as exceptions
-import art.rhevm_api.tests_lib.high_level.networks as hl_networks
 
 logger = logging.getLogger("ArbitraryVlanDeviceName_Init")
 
 
 def setup_package():
     """
-    Prepare environment
+    Setting passwordless ssh and then disabling sasl in libvirt
     """
-    if network.config.GOLDEN_ENV:
-        logger.info("Running on golden env, calling network_cleanup")
-        network.network_cleanup()
-
-    else:
-        logger.info("Create setup with datacenter, cluster and host")
-        if not hl_networks.create_basic_setup(
-            datacenter=network.config.DC_NAME[0],
-            storage_type=network.config.STORAGE_TYPE,
-            version=network.config.COMP_VERSION,
-            cluster=network.config.CLUSTER_NAME[0],
-            cpu=network.config.CPU_NAME, host=network.config.HOSTS[0],
-            host_password=network.config.HOSTS_PW
-        ):
-            raise exceptions.NetworkException("Failed to create setup")
-
+    network.network_cleanup()
     logger.info(
         "Setting passwordless ssh from engine (%s) to host (%s)",
         network.config.ENGINE_HOST.ip, network.config.VDS_HOSTS[0].ip
@@ -59,19 +43,8 @@ def setup_package():
 
 def teardown_package():
     """
-    Cleans the environment
+    Enabling sasl in libvirt
     """
-    if network.config.GOLDEN_ENV:
-        logger.info("Running on golden env, no teardown")
-
-    else:
-        if not hl_networks.remove_basic_setup(
-            datacenter=network.config.DC_NAME[0],
-            cluster=network.config.CLUSTER_NAME[0],
-            hosts=[network.config.HOSTS[0]]
-        ):
-            logger.error("Failed to remove setup")
-
     logger.info("Enabling sasl in libvirt")
     if not helper.set_libvirtd_sasl(host_obj=network.config.VDS_HOSTS[0]):
         logger.error("Failed to enable sasl on %s", network.config.HOSTS[0])
