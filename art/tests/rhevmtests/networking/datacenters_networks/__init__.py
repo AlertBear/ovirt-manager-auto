d10e22155e78096d4572fb5c4b81a56471d371db
obensass <obensass@redhat.com>
2015-08-26 10:17:22 +0300

    Network: run datacenters_networks only on GE
    Change-Id: I9eacd3b6cc563d6eb0751277f164e1b95f5c5899
    Reviewed-on: https://code.engineering.redhat.com/gerrit/56352
    Reviewed-by: Meni Yakove <myakove@redhat.com>
    Tested-by: Meni Yakove <myakove@redhat.com>
    Reviewed-by: Kobi Hakimi <khakimi@redhat.com>

--- art/tests/rhevmtests/networking/datacenters_networks/__init__.py
+++ art/tests/rhevmtests/networking/datacenters_networks/__init__.py
@@ -2,11 +2,11 @@
 DataCenter Networks feature test
 """
 import logging
-from rhevmtests.networking import config, network_cleanup
-from art.rhevm_api.tests_lib.high_level.networks import(
-    create_basic_setup, remove_basic_setup
-)
-from art.test_handler.exceptions import NetworkException
+import rhevmtests.networking as networking
+import rhevmtests.networking.config as config
+import art.rhevm_api.tests_lib.high_level.networks as hl_networks
+import art.test_handler.exceptions as exceptions
+
 logger = logging.getLogger("DC_Networks_Init")
 #################################################
 DC_NAMES = [config.DC_NAME[0], "DC_NET_DC2"]
@@ -16,37 +16,20 @@ def setup_package():
     """
     Prepare environment
     """
-    if config.GOLDEN_ENV:
-        network_cleanup()
-        if not create_basic_setup(
-                datacenter=DC_NAMES[1],
-                storage_type=config.STORAGE_TYPE,
-                version=config.COMP_VERSION
-        ):
-            raise NetworkException("Failed to create setup")
-
-    else:
-        for dc in DC_NAMES:
-            logger.info("Create datacenter %s", dc)
-            if not create_basic_setup(
-                    datacenter=dc,
-                    storage_type=config.STORAGE_TYPE,
-                    version=config.COMP_VERSION
-            ):
-                raise NetworkException(
-                    "Failed to create setup for DC %s" % dc
-                )
+    networking.network_cleanup()
+    logger.info("Create basic setup")
+    if not hl_networks.create_basic_setup(
+        datacenter=DC_NAMES[1],
+        storage_type=config.STORAGE_TYPE,
+        version=config.COMP_VERSION
+    ):
+        raise exceptions.NetworkException("Failed to create setup")
 
 
 def teardown_package():
     """
     Cleans environment
     """
-    if config.GOLDEN_ENV:
-        if not remove_basic_setup(datacenter=DC_NAMES[1]):
-            logger.error("Cannot remove DC")
-
-    else:
-        for dc in DC_NAMES:
-            if not remove_basic_setup(datacenter=dc):
-                logger.error("Cannot remove DCs")
+    logger.info("Remove basic setup")
+    if not hl_networks.remove_basic_setup(datacenter=DC_NAMES[1]):
+        logger.error("Cannot remove DC")
