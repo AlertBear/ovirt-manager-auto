fc349e9b33d020921d16eebd9fceba45bb227641
obensass <obensass@redhat.com>
2015-08-26 15:46:22 +0300

    Network: run int_fault_event only on GE
    Change-Id: Iedc2244581553c8da5ecead4d239923566dd8cff
    Reviewed-on: https://code.engineering.redhat.com/gerrit/56411
    Reviewed-by: Meni Yakove <myakove@redhat.com>
    Tested-by: Ori Ben Sasson <obensass@redhat.com>
    Reviewed-by: Kobi Hakimi <khakimi@redhat.com>

--- art/tests/rhevmtests/networking/int_fault_event/__init__.py
+++ art/tests/rhevmtests/networking/int_fault_event/__init__.py
@@ -6,44 +6,15 @@ Display of NIC Slave/Bond fault on RHEV-M Event Log feature test
 """
 
 import logging
-import config as c
-from rhevmtests.networking import network_cleanup
-import art.test_handler.exceptions as exceptions
-import art.rhevm_api.tests_lib.high_level.networks as hl_networks
+import config as config
+import rhevmtests.networking as networking
 
 logger = logging.getLogger("Int_Fault_Event_Init")
 
 
 def setup_package():
     """
-    Prepare environment
+    Running cleanup
     """
-    c.HOST_NICS = c.VDS_HOSTS_0.nics
-    if c.GOLDEN_ENV:
-        logger.info("Running on golden env, no setup")
-        network_cleanup()
-
-    else:
-        logger.info("Create setup with datacenter, cluster and host")
-        if not hl_networks.create_basic_setup(
-            datacenter=c.DC_NAME[0], storage_type=c.STORAGE_TYPE,
-            version=c.COMP_VERSION, cluster=c.CLUSTER_NAME[0],
-            cpu=c.CPU_NAME, host=c.HOSTS[0],
-            host_password=c.HOSTS_PW
-        ):
-            raise exceptions.NetworkException("Failed to create setup")
-
-
-def teardown_package():
-    """
-    Cleans the environment
-    """
-    if c.GOLDEN_ENV:
-        logger.info("Running on golden env, no teardown")
-
-    else:
-        if not hl_networks.remove_basic_setup(
-            datacenter=c.DC_NAME[0], cluster=c.CLUSTER_NAME[0],
-            hosts=c.HOSTS[0]
-        ):
-                logger.error("Failed to remove setup")
+    config.HOST_NICS = config.VDS_HOSTS_0.nics
+    networking.network_cleanup()
