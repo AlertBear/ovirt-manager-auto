ad5029b0addb6970c6e2cb522dace0f2fda5ed86
obensass <obensass@redhat.com>
2015-08-27 09:25:53 +0300

    Network: run io_test only on GE
    Change-Id: I8bc3bee6fb522e9f9715594996fb25ffd268e2d6
    Reviewed-on: https://code.engineering.redhat.com/gerrit/56469
    Tested-by: Ori Ben Sasson <obensass@redhat.com>
    Reviewed-by: Meni Yakove <myakove@redhat.com>
    Reviewed-by: Kobi Hakimi <khakimi@redhat.com>

--- art/tests/rhevmtests/networking/io_test/__init__.py
+++ art/tests/rhevmtests/networking/io_test/__init__.py
@@ -2,11 +2,8 @@
 IO feature test
 """
 import logging
-from rhevmtests.networking import config, network_cleanup
-from art.rhevm_api.tests_lib.high_level.networks import(
-    create_basic_setup, remove_basic_setup
-)
-from art.test_handler.exceptions import NetworkException
+import rhevmtests.networking as networking
+
 
 logger = logging.getLogger("IO_Test_Init")
 #################################################
@@ -14,36 +11,6 @@ logger = logging.getLogger("IO_Test_Init")
 
 def setup_package():
     """
-    Prepare environment:
-    create 1 Data Center
-    create 1 Cluster
-    add 1 Host
+    running cleanup
     """
-    if config.GOLDEN_ENV:
-        logger.info("Running on golden env, no setup")
-        network_cleanup()
-
-    else:
-        logger.info("Create setup with datacenter, cluster and host")
-        if not create_basic_setup(
-                datacenter=config.DC_NAME[0], storage_type=config.STORAGE_TYPE,
-                version=config.COMP_VERSION, cluster=config.CLUSTER_NAME[0],
-                cpu=config.CPU_NAME, host=config.HOSTS[0],
-                host_password=config.HOSTS_PW
-        ):
-            raise NetworkException("Failed to create setup")
-
-
-def teardown_package():
-    """
-    Cleans environment by removing Host, Cluster and DC from the setup
-    """
-    if config.GOLDEN_ENV:
-        logger.info("Running on golden env, no teardown")
-        return
-
-    if not remove_basic_setup(
-            datacenter=config.DC_NAME[0], cluster=config.CLUSTER_NAME[0],
-            hosts=[config.HOSTS[0]]
-    ):
-        logger.error("Failed to remove setup")
+    networking.network_cleanup()
