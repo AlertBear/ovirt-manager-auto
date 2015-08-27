474466c92fd6755b8c0f97c19da61f30b0b39aa2
obensass <obensass@redhat.com>
2015-08-27 15:39:33 +0300

    Network: run network_filter only on GE
    Change-Id: Ied14088d626e9e8b80ec454a9c68b3dde8845ae6
    Reviewed-on: https://code.engineering.redhat.com/gerrit/56517
    Tested-by: Ori Ben Sasson <obensass@redhat.com>
    Reviewed-by: Meni Yakove <myakove@redhat.com>
    Reviewed-by: Tareq Alayan <talayan@redhat.com>

--- art/tests/rhevmtests/networking/network_filter/__init__.py
+++ art/tests/rhevmtests/networking/network_filter/__init__.py
@@ -3,11 +3,10 @@ NetworkFilter Test
 """
 
 import logging
-from art.rhevm_api.tests_lib.high_level.datacenters import clean_datacenter
-from rhevmtests.networking import config, network_cleanup
-from art.rhevm_api.tests_lib.high_level.networks import prepareSetup
-from art.test_handler.exceptions import NetworkException
-from art.rhevm_api.tests_lib.low_level import vms
+import rhevmtests.networking as networking
+import rhevmtests.networking.config as config
+import art.test_handler.exceptions as exceptions
+import art.rhevm_api.tests_lib.low_level.vms as ll_vms
 import rhevmtests.networking.helper as net_help
 
 logger = logging.getLogger("Network_Filter_Init")
@@ -17,63 +16,32 @@ logger = logging.getLogger("Network_Filter_Init")
 
 def setup_package():
     """
-    Prepare environment
+    Running cleanup and starting VM
     """
-    if config.GOLDEN_ENV:
-        network_cleanup()
-        logger.info(
-            "Running on golden env, starting VM %s on host %s",
-            config.VM_NAME[0], config.HOSTS[0]
+
+    networking.network_cleanup()
+    logger.info(
+        "Starting VM %s on host %s",
+        config.VM_NAME[0], config.HOSTS[0]
+    )
+
+    if not net_help.run_vm_once_specific_host(
+            vm=config.VM_NAME[0], host=config.HOSTS[0], wait_for_ip=True
+    ):
+        raise exceptions.NetworkException(
+            "Cannot start VM %s on host %s" %
+            (config.VM_NAME[0], config.HOSTS[0])
         )
-
-        if not net_help.run_vm_once_specific_host(
-                vm=config.VM_NAME[0], host=config.HOSTS[0]
-        ):
-            raise NetworkException(
-                "Cannot start VM %s on host %s" %
-                (config.VM_NAME[0], config.HOSTS[0])
-            )
-        if not vms.waitForVMState(vm=config.VM_NAME[0]):
-            raise NetworkException(
-                "VM %s did not come up" % config.VM_NAME[0]
-            )
-
-    else:
-        logger.info("Creating data center, cluster, adding host and storage")
-        if not prepareSetup(hosts=config.VDS_HOSTS[0],
-                            cpuName=config.CPU_NAME,
-                            username=config.HOSTS_USER,
-                            password=config.HOSTS_PW,
-                            datacenter=config.DC_NAME[0],
-                            storageDomainName=config.STORAGE_NAME[0],
-                            storage_type=config.STORAGE_TYPE,
-                            cluster=config.CLUSTER_NAME[0],
-                            lun_address=config.LUN_ADDRESS[0],
-                            lun_target=config.LUN_TARGET[0],
-                            luns=config.LUN[0], version=config.COMP_VERSION,
-                            vmName=config.VM_NAME[0],
-                            vm_password=config.VMS_LINUX_PW,
-                            mgmt_network=config.MGMT_BRIDGE,
-                            auto_nics=[0]):
-            raise NetworkException("Cannot create setup")
 
 
 def teardown_package():
     """
-    Cleans the environment
+    Stopping VM
     """
-    if config.GOLDEN_ENV:
-        logger.info(
-            "Running on golden env, stopping VM %s", config.VM_NAME[0]
+    logger.info(
+        "Stopping VM %s", config.VM_NAME[0]
+    )
+    if not ll_vms.stopVm(True, vm=config.VM_NAME[0]):
+        logger.error(
+            "Failed to stop VM: %s", config.VM_NAME[0]
         )
-        if not vms.stopVm(True, vm=config.VM_NAME[0]):
-            logger.error(
-                "Failed to stop VM: %s", config.VM_NAME[0]
-            )
-
-    else:
-        if not clean_datacenter(
-                positive=True, datacenter=config.DC_NAME[0],
-                vdc=config.VDC_HOST, vdc_password=config.VDC_ROOT_PASSWORD
-        ):
-            logger.error("Cannot remove setup")
