d6ebe861d11d43fdd1fc776303c162c2b5963ff1
obensass <obensass@redhat.com>
2015-08-27 10:11:28 +0300

    Network: run linking only on GE
    Change-Id: Ieaf10c34c7ed34ac3432f97d20c653ab6425b8cc
    Reviewed-on: https://code.engineering.redhat.com/gerrit/56474
    Tested-by: Ori Ben Sasson <obensass@redhat.com>
    Reviewed-by: Genadi Chereshnya <gcheresh@redhat.com>

--- art/tests/rhevmtests/networking/linking/__init__.py
+++ art/tests/rhevmtests/networking/linking/__init__.py
@@ -6,15 +6,11 @@ Linking feature test
 """
 
 import logging
-
-from rhevmtests.networking import config, network_cleanup
-from art.rhevm_api.tests_lib.high_level.datacenters import clean_datacenter
-from art.test_handler.exceptions import NetworkException
-from art.rhevm_api.tests_lib.low_level.vms import addVm
-from art.rhevm_api.tests_lib.low_level import vms
-from art.rhevm_api.tests_lib.high_level.networks import(
-    createAndAttachNetworkSN, prepareSetup, remove_net_from_setup
-)
+import rhevmtests.networking as networking
+import rhevmtests.networking.config as config
+import art.test_handler.exceptions as exceptions
+import art.rhevm_api.tests_lib.low_level.vms as ll_vms
+import art.rhevm_api.tests_lib.high_level.networks as hl_networks
 import rhevmtests.networking.helper as net_help
 
 logger = logging.getLogger("Linking_Init")
@@ -43,79 +39,40 @@ def setup_package():
             "vlan_id": config.VLAN_ID[4], "nic": 1, "required": "false"
         }
     }
-    if config.GOLDEN_ENV:
-        network_cleanup()
-        logger.info(
-            "Running on golden env, setting up only networks and starting "
-            " VM %s at host %s", config.VM_NAME[0], config.HOSTS[0]
+
+    networking.network_cleanup()
+    logger.info(
+        "Start VM %s on host %s", config.VM_NAME[0], config.HOSTS[0]
+    )
+
+    if not net_help.run_vm_once_specific_host(
+        vm=config.VM_NAME[0], host=config.HOSTS[0], wait_for_ip=True
+    ):
+        raise exceptions.NetworkException(
+            "Cannot start VM %s at host %s" %
+            (config.VM_NAME[0], config.HOSTS[0])
         )
 
-        if not net_help.run_vm_once_specific_host(
-                vm=config.VM_NAME[0], host=config.HOSTS[0], wait_for_ip=True
-        ):
-            raise NetworkException(
-                "Cannot start VM %s at host %s" %
-                (config.VM_NAME[0], config.HOSTS[0])
-            )
-        if not vms.waitForVMState(vm=config.VM_NAME[0]):
-            raise NetworkException("VM %s did not come up" % config.VM_NAME[0])
-
-    else:
-        if not prepareSetup(
-            hosts=config.VDS_HOSTS[0], cpuName=config.CPU_NAME,
-            username=config.HOSTS_USER, password=config.HOSTS_PW,
-            datacenter=config.DC_NAME[0],
-            storageDomainName=config.STORAGE_NAME[0],
-            storage_type=config.STORAGE_TYPE, cluster=config.CLUSTER_NAME[0],
-            lun_address=config.LUN_ADDRESS[0], lun_target=config.LUN_TARGET[0],
-            luns=config.LUN[0], version=config.COMP_VERSION,
-            vmName=config.VM_NAME[0], template_name=config.TEMPLATE_NAME[0],
-            vm_password=config.VMS_LINUX_PW, mgmt_network=config.MGMT_BRIDGE,
-            auto_nics=[0]
-        ):
-            raise NetworkException("Cannot create setup")
-
-        if not addVm(
-                True, name=config.VM_NAME[1], cluster=config.CLUSTER_NAME[0],
-                template=config.TEMPLATE_NAME[0],
-                display_type=config.DISPLAY_TYPE
-        ):
-            raise NetworkException(
-                "Cannot create VM %s from template" % config.VM_NAME[1]
-            )
-
-    if not createAndAttachNetworkSN(
-            data_center=config.DC_NAME[0], cluster=config.CLUSTER_NAME[0],
-            host=config.VDS_HOSTS[0], network_dict=local_dict,
-            auto_nics=[0, 1]
+    if not hl_networks.createAndAttachNetworkSN(
+        data_center=config.DC_NAME[0], cluster=config.CLUSTER_NAME[0],
+        host=config.VDS_HOSTS[0], network_dict=local_dict,
+        auto_nics=[0, 1]
     ):
-        raise NetworkException("Cannot create and attach networks")
+        raise exceptions.NetworkException("Cannot create and attach networks")
 
 
 def teardown_package():
     """
     Cleans the environment
     """
-    if config.GOLDEN_ENV:
-        logger.info(
-            "Running on golden env, removing networks and stopping VM %s",
-            config.VM_NAME[0]
-        )
-        if not vms.stopVm(True, vm=config.VM_NAME[0]):
-            logger.error(
-                "Failed to stop VM: %s", config.VM_NAME[0]
-            )
 
-        if not remove_net_from_setup(
-                host=config.VDS_HOSTS[0], auto_nics=[0],
-                data_center=config.DC_NAME[0], all_net=True,
-                mgmt_network=config.MGMT_BRIDGE
-        ):
-            logger.error("Failed to remove networks from setup")
+    logger.info("Stop VM %s and remove VLAN networks", config.VM_NAME[0])
+    if not ll_vms.stopVm(True, vm=config.VM_NAME[0]):
+        logger.error("Failed to stop VM: %s", config.VM_NAME[0])
 
-    else:
-        if not clean_datacenter(
-                positive=True, datacenter=config.DC_NAME[0],
-                vdc=config.VDC_HOST, vdc_password=config.VDC_ROOT_PASSWORD
-        ):
-            logger.error("Cannot remove setup")
+    if not hl_networks.remove_net_from_setup(
+        host=config.VDS_HOSTS[0], auto_nics=[0],
+        data_center=config.DC_NAME[0], all_net=True,
+        mgmt_network=config.MGMT_BRIDGE
+    ):
+        logger.error("Failed to remove all networks beside MGMT")
