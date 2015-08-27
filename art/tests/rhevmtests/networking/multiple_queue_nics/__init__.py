0ac48e9cf84ffd6fc21641b5febc7d2dde5f1a70
obensass <obensass@redhat.com>
2015-08-27 14:35:54 +0300

    Network: run multiple_queue_nics
    Change-Id: I7913d11dcb5bf9bb845f2a09083a43a4cb4e7fee
    Reviewed-on: https://code.engineering.redhat.com/gerrit/56510
    Reviewed-by: Meni Yakove <myakove@redhat.com>
    Tested-by: Ori Ben Sasson <obensass@redhat.com>
    Reviewed-by: Kobi Hakimi <khakimi@redhat.com>

--- art/tests/rhevmtests/networking/multiple_queue_nics/__init__.py
+++ art/tests/rhevmtests/networking/multiple_queue_nics/__init__.py
@@ -3,12 +3,10 @@ multiple_queue_nics job
 """
 
 import logging
-from art.rhevm_api.tests_lib.high_level.datacenters import clean_datacenter
-from art.rhevm_api.tests_lib.low_level.vms import stopVm
-from art.rhevm_api.utils.test_utils import set_engine_properties
-from rhevmtests.networking import config, network_cleanup
-from art.test_handler.exceptions import NetworkException
-from art.rhevm_api.tests_lib.high_level.networks import prepareSetup
+import art.rhevm_api.utils.test_utils as test_utils
+import rhevmtests.networking as networking
+import rhevmtests.networking.config as config
+import art.test_handler.exceptions as exceptions
 
 logger = logging.getLogger("Multiple_Queues_Nics_Init")
 VER = config.COMP_VERSION
@@ -19,7 +17,7 @@ def setup_package():
     """
     Prepare environment
     """
-    network_cleanup()
+    networking.network_cleanup()
     logger.info(
         "Configuring engine to support queues for %s version", VER
     )
@@ -27,25 +25,12 @@ def setup_package():
         "CustomDeviceProperties='{type=interface;prop={queues=[1-9][0-9]*}}'",
         "'--cver=%s'" % VER
     ]
-    if not set_engine_properties(engine_obj=config.ENGINE, param=param):
-        raise NetworkException("Failed to enable queue via engine-config")
-
-    if not config.GOLDEN_ENV:
-        if not prepareSetup(
-            hosts=config.VDS_HOSTS, cpuName=config.CPU_NAME,
-            username=config.HOSTS_USER, password=config.HOSTS_PW,
-            datacenter=config.DC_NAME[0],
-            storageDomainName=config.STORAGE_NAME[0],
-            storage_type=config.STORAGE_TYPE, cluster=config.CLUSTER_NAME[0],
-            lun_address=config.LUN_ADDRESS[0], lun_target=config.LUN_TARGET[0],
-            luns=config.LUN[0], version=config.COMP_VERSION,
-            vmName=config.VM_NAME[0], vm_password=config.VMS_LINUX_PW,
-            mgmt_network=config.MGMT_BRIDGE
-        ):
-            raise NetworkException("Cannot create setup")
-
-        if not stopVm(positive=True, vm=config.VM_NAME[0]):
-            raise NetworkException("Failed to stop %s" % config.VM_NAME[0])
+    if not test_utils.set_engine_properties(
+            engine_obj=config.ENGINE, param=param
+    ):
+        raise exceptions.NetworkException(
+            "Failed to enable queue via engine-config"
+        )
 
 
 def teardown_package():
@@ -54,14 +39,9 @@ def teardown_package():
     """
     logger.info("Removing queues support from engine for %s version", VER)
     param = ["CustomDeviceProperties=''", "'--cver=%s'" % VER]
-    if not set_engine_properties(engine_obj=config.ENGINE, param=param):
+    if not test_utils.set_engine_properties(
+            engine_obj=config.ENGINE, param=param
+    ):
         logger.error(
             "Failed to remove queues support via engine-config"
         )
-
-    if not config.GOLDEN_ENV:
-        if not clean_datacenter(
-            positive=True, datacenter=config.DC_NAME[0],
-            vdc=config.VDC_HOST, vdc_password=config.VDC_ROOT_PASSWORD
-        ):
-            logger.error("Cannot remove setup")
