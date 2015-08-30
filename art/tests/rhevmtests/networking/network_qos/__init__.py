86df10017fc2283aeb13dc807fb25f7d11ab4cc2
obensass <obensass@redhat.com>
2015-08-30 10:16:24 +0300

    Network: run network_qos only on GE
    Change-Id: I64e4c164f1d26ac404efe396690423fb2dd645c1
    Reviewed-on: https://code.engineering.redhat.com/gerrit/56635
    Tested-by: Ori Ben Sasson <obensass@redhat.com>
    Reviewed-by: Genadi Chereshnya <gcheresh@redhat.com>

--- art/tests/rhevmtests/networking/network_qos/__init__.py
+++ art/tests/rhevmtests/networking/network_qos/__init__.py
@@ -2,16 +2,14 @@
 # -*- coding: utf-8 -*-
 
 """
-Network QOS feature test
+Network QOS feature init
 """
 
 import logging
-from art.rhevm_api.tests_lib.high_level.datacenters import clean_datacenter
-from art.rhevm_api.tests_lib.low_level.vms import addVm
-from rhevmtests.networking import config, network_cleanup
-from art.test_handler.exceptions import NetworkException
-from art.rhevm_api.tests_lib.high_level.networks import prepareSetup
-from art.rhevm_api.tests_lib.low_level import vms
+import rhevmtests.networking as networking
+import rhevmtests.networking.config as config
+import art.test_handler.exceptions as exceptions
+import art.rhevm_api.tests_lib.low_level.vms as ll_vms
 import rhevmtests.networking.helper as net_help
 
 logger = logging.getLogger("Network_VNIC_QoS_Init")
@@ -21,69 +19,30 @@ logger = logging.getLogger("Network_VNIC_QoS_Init")
 
 def setup_package():
     """
-    Prepare environment
+    Running cleanup and starting vm
     """
-    if config.GOLDEN_ENV:
-        network_cleanup()
-        logger.info(
-            "Running on golden env, starting 1 VM %s on host %s",
-            config.VM_NAME[0], config.HOSTS[0]
-        )
-        if not net_help.run_vm_once_specific_host(
-            vm=config.VM_NAME[0], host=config.HOSTS[0]
-        ):
-            raise NetworkException(
-                "Cannot start VM %s on host %s" %
-                (config.VM_NAME[0], config.HOSTS[0])
-            )
-        if not vms.waitForVMState(vm=config.VM_NAME[0]):
-            raise NetworkException(
-                "VM %s did not come up" % config.VM_NAME[0]
-            )
-    else:
-        if not prepareSetup(
-            hosts=config.VDS_HOSTS, cpuName=config.CPU_NAME,
-            username=config.HOSTS_USER, password=config.HOSTS_PW,
-            datacenter=config.DC_NAME[0],
-            storageDomainName=config.STORAGE_NAME[0],
-            storage_type=config.STORAGE_TYPE,
-            cluster=config.CLUSTER_NAME[0],
-            lun_address=config.LUN_ADDRESS[0],
-            lun_target=config.LUN_TARGET[0],
-            luns=config.LUN[0], version=config.COMP_VERSION,
-            placement_host=config.HOSTS[0], vmName=config.VM_NAME[0],
-            template_name=config.TEMPLATE_NAME[0],
-            vm_password=config.VMS_LINUX_PW, mgmt_network=config.MGMT_BRIDGE
-        ):
-            raise NetworkException("Cannot create setup")
 
-        logger.info("Add VM %s to the setup", config.VM_NAME[1])
-        if not addVm(
-            True, name=config.VM_NAME[1], cluster=config.CLUSTER_NAME[0],
-            template=config.TEMPLATE_NAME[0], display_type=config.DISPLAY_TYPE,
-            placement_host=config.HOSTS[0]
-        ):
-            raise NetworkException("Cannot create VM %s from template" %
-                                   config.VM_NAME[1])
+    networking.network_cleanup()
+    logger.info(
+        "Starting VM %s on host %s", config.VM_NAME[0], config.HOSTS[0]
+    )
+    if not net_help.run_vm_once_specific_host(
+            vm=config.VM_NAME[0], host=config.HOSTS[0], wait_for_ip=True
+    ):
+        raise exceptions.NetworkException(
+            "Cannot start VM %s on host %s" %
+            (config.VM_NAME[0], config.HOSTS[0])
+        )
 
 
 def teardown_package():
     """
-    Cleans the environment
+    Stopping VM
     """
-    if config.GOLDEN_ENV:
-        logger.info(
-            "Running on golden env, stopping VM %s", config.VM_NAME[0]
-        )
 
-        if not vms.stopVms(vms=config.VM_NAME[0]):
-            logger.error(
-                "Failed to stop VM: %s", config.VM_NAME[0]
-            )
+    logger.info(
+        "Stopping VM %s", config.VM_NAME[0]
+    )
 
-    else:
-        if not clean_datacenter(
-            positive=True, datacenter=config.DC_NAME[0], vdc=config.VDC_HOST,
-            vdc_password=config.VDC_ROOT_PASSWORD
-        ):
-            logger.error("Cannot remove setup")
+    if not ll_vms.stopVms(vms=config.VM_NAME[0]):
+        logger.error("Failed to stop VM: %s", config.VM_NAME[0])
