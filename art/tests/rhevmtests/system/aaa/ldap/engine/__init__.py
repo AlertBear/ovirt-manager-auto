549fb8fae774082dd2c5ce13fb9b69b86efd8b17
Ondra Machacek <omachace@redhat.com>
2015-10-23 10:04:17 +0200

    system: aaa-ldap: move auth test to subdir
    Change-Id: If37b0410962db13cd685ab30ba2dbb106de3e043
    Reviewed-on: https://code.engineering.redhat.com/gerrit/60034
    Tested-by: Ondra Machacek <omachace@redhat.com>
    Reviewed-by: Ondra Machacek <omachace@redhat.com>

---
+++ art/tests/rhevmtests/system/aaa/ldap/engine/__init__.py
@@ -0,0 +1,35 @@
+import logging
+import os
+
+from rhevmtests.system.aaa.ldap import config, common
+
+
+LOGGER = logging.getLogger(__name__)
+
+
+def setup_module():
+    test_dir = os.path.dirname(os.path.abspath(__file__))
+    tests = set([
+        f[:f.rfind('.')] for f in os.listdir(test_dir) if f.startswith('test_')
+    ])
+    for name in tests:
+        common.prepareExtensions(
+            name,
+            config.ENGINE_EXTENSIONS_DIR,
+            config.EXTENSIONS,
+            chown='ovirt',
+            clean=False,
+            enable=False,
+        )
+    common.createTrustStore(
+        config.ADW2K12_DOMAINS, config.TRUSTSTORE, config.TRUSTSTORE_PASSWORD
+    )
+    common.enableExtensions(config.OVIRT_SERVICE, config.ENGINE_HOST)
+    common.loginAsAdmin()
+
+
+def teardown_module():
+    common.removeTrustStore(config.TRUSTSTORE)
+    common.cleanExtDirectory(config.ENGINE_EXTENSIONS_DIR)
+    common.cleanExtDirectory(config.AAA_DIR)
+    common.enableExtensions(config.OVIRT_SERVICE, config.ENGINE_HOST)
