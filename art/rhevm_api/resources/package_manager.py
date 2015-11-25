d143927f9e1895d55ec65f17cf84080dbc7e8eb3
Ondra Machacek <omachace@redhat.com>
2015-11-25 18:15:40 +0100

    resources: adding APT package manager
    Change-Id: I19ead551b9fa76a045c1744ac9894a0cb2e81017
    Reviewed-on: https://code.engineering.redhat.com/gerrit/62324
    Tested-by: Ondra Machacek <omachace@redhat.com>
    Reviewed-by: Meni Yakove <myakove@redhat.com>
    Reviewed-by: Lukas Bednar <lbednar@redhat.com>

--- art/rhevm_api/resources/package_manager.py
+++ art/rhevm_api/resources/package_manager.py
@@ -2,6 +2,7 @@ from art.rhevm_api.resources.service import Service
 
 YUM_MANAGER_CLASS = 'YumPackageManager'
 RPM_MANAGER_CLASS = 'RPMPackageManager'
+APT_MANAGER_CLASS = 'APTPackageManager'
 
 
 class PackageManager(Service):
@@ -10,15 +11,24 @@ class PackageManager(Service):
     """
     exist_command_d = {
         YUM_MANAGER_CLASS: ['yum', 'list', 'installed'],
-        RPM_MANAGER_CLASS: ['rpm', '-q']
+        RPM_MANAGER_CLASS: ['rpm', '-q'],
+        # FIXME: Once apt will return correct return codes fix this
+        APT_MANAGER_CLASS: ['apt', 'list', '--installed', '|', 'grep'],
     }
     install_command_d = {
         YUM_MANAGER_CLASS: ['yum', 'install', '-y'],
-        RPM_MANAGER_CLASS: ['rpm', '-i']
+        RPM_MANAGER_CLASS: ['rpm', '-i'],
+        APT_MANAGER_CLASS: ['apt', 'install', '-y'],
     }
     erase_command_d = {
         YUM_MANAGER_CLASS: ['yum', 'erase', '-y'],
-        RPM_MANAGER_CLASS: ['rpm', '-e']
+        RPM_MANAGER_CLASS: ['rpm', '-e'],
+        APT_MANAGER_CLASS: ['apt', 'remove', '-y'],
+    }
+    update_command_d = {
+        YUM_MANAGER_CLASS: ['yum', 'update', '-y'],
+        RPM_MANAGER_CLASS: ['rpm', '-U'],
+        APT_MANAGER_CLASS: ['apt', 'update', '-y'],
     }
 
     def __init__(self, host):
@@ -110,6 +120,27 @@ class PackageManager(Service):
         )
         return True
 
+    def update(self, packages=None):
+        """
+        Updated specified packages, or all available system updates
+        if no packages are specified
+
+        __author__ = "omachace"
+        :param packages: Packages to be updated, if empty, update system
+        :type packages: list
+        :return: True when updates succeed, False otherwise
+        :rtype: bool
+        """
+        cmd = list(self.update_command_d[self.cls_name])
+        if packages:
+            cmd.extend(packages)
+            self.logger.info(
+                "Update packages %s on host %s", packages, self.host
+            )
+        else:
+            self.logger.info("Updating system on host %s", self.host)
+        return self._run_command_on_host(cmd)
+
 
 class YumPackageManager(PackageManager):
     """
@@ -121,5 +152,12 @@ class YumPackageManager(PackageManager):
 class RPMPackageManager(PackageManager):
     """
     RPM package manager class
+    """
+    pass
+
+
+class APTPackageManager(PackageManager):
+    """
+    APT package manager class
     """
     pass
