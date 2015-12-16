88a0d735b62f74597d40569bac28c9df2762b965
Gonzalo Rafuls <grafuls@redhat.com>
2015-12-16 09:26:47 +0100

    System: fix for hooks vm creation
    Change-Id: Ia558f866053d54941e680d9f07e978135e476199
    Reviewed-on: https://code.engineering.redhat.com/gerrit/63900
    Reviewed-by: Lukas Bednar <lbednar@redhat.com>
    Reviewed-by: Pavel Novotny <pnovotny@redhat.com>
    Reviewed-by: Israel Pinto <ipinto@redhat.com>
    Reviewed-by: Artyom Lukianov <alukiano@redhat.com>
    Tested-by: Gonzalo Rafuls <grafuls@redhat.com>
    Reviewed-by: Ondra Machacek <omachace@redhat.com>

--- art/tests/rhevmtests/system/hooks/config.py
+++ art/tests/rhevmtests/system/hooks/config.py
@@ -8,5 +8,6 @@ from rhevmtests.system.config import *  # flake8: noqa
 
 VM_UP = ENUMS['vm_state_up']
 HOOKS_VM_NAME = 'test_vm_hooks'
+DISPLAY_TYPE = ENUMS['display_type_vnc']
 if not GOLDEN_ENV:
     TEMPLATE_NAME = [PARAMETERS.get('template', 'hooks_template')]
