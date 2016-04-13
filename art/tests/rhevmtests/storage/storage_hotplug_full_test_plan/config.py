35290c62eccb5edc20821159affd921255e1c59e
Raz Tamir <ratamir@redhat.com>
2016-04-13 11:53:09 +0300

    Storage: Fix storage_hotplug_full_test
     Make sure the TESTNAME parameter is specific to the plan
    
    Change-Id: I8642012fdef9750ae0764eb88c9b7304ea4e00e6
    Reviewed-on: https://code.engineering.redhat.com/gerrit/72073
    Tested-by: Raz Tamir <ratamir@redhat.com>
    Reviewed-by: Carlos Mestre Gonzalez <cmestreg@redhat.com>
    Reviewed-by: Raz Tamir <ratamir@redhat.com>

--- art/tests/rhevmtests/storage/storage_hotplug_full_test_plan/config.py
+++ art/tests/rhevmtests/storage/storage_hotplug_full_test_plan/config.py
@@ -4,6 +4,7 @@ Config module for storage hotplug full test plan
 from rhevmtests.storage.config import *  # flake8: noqa
 
 CLASS_VM_NAME_FORMAT = "%s_%s_vm"
+TESTNAME = 'hotplug_full_test_plan'
 VM_NAME = TESTNAME + "_%s_vm"
 DISK_NAME_FORMAT = "%s_%s_%s_disk"
 TEMPLATE_NAME = "hotplug_full_test_template_%s"
