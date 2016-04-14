fa81e6e42659355fe46dfc6517263b6586d346f2
Tareq Alayan <talayan@redhat.com>
2016-04-14 14:38:53 +0300

    network: skip if no jumbo frame support
    Change-Id: I5b1c59cdce4dd7c3dc40bc28d151adfbbeaf54d8
    Reviewed-on: https://code.engineering.redhat.com/gerrit/72186
    Reviewed-by: Nelly Credi <ncredi@redhat.com>
    Tested-by: Tareq Alayan <talayan@redhat.com>
    Reviewed-by: Petr Balogh <pbalogh@redhat.com>
    Reviewed-by: Meni Yakove <myakove@redhat.com>

--- art/tests/rhevmtests/networking/jumbo_frames/jumbo_test.py
+++ art/tests/rhevmtests/networking/jumbo_frames/jumbo_test.py
@@ -91,6 +91,12 @@ def teardown_module():
 
 
 @attr(tier=2)
+@pytest.mark.skipif(
+    conf.NOT_4_NICS_HOSTS, reason=conf.NOT_4_NICS_HOST_SKIP_MSG
+)
+@pytest.mark.skipif(
+    conf.NO_JUMBO_FRAME_SUPPORT, reason=conf.NO_JUMBO_FRAME_SUPPORT_SKIP_MSG
+)
 class TestJumboFramesTestCaseBase(NetworkTest):
     """
     Base class which provides teardown class method for each test case
@@ -196,10 +202,6 @@ class TestJumboFramesCase02(TestJumboFramesTestCaseBase):
         )
 
 
-@attr(tier=2)
-@pytest.mark.skipif(
-    conf.NOT_4_NICS_HOSTS, reason=conf.NOT_4_NICS_HOST_SKIP_MSG
-)
 class TestJumboFramesCase03(TestJumboFramesTestCaseBase):
     """
     Check connectivity between two hosts
@@ -333,10 +335,6 @@ class TestJumboFramesCase03(TestJumboFramesTestCaseBase):
         super(TestJumboFramesCase03, cls).teardown_class()
 
 
-@attr(tier=2)
-@pytest.mark.skipif(
-    conf.NOT_4_NICS_HOSTS, reason=conf.NOT_4_NICS_HOST_SKIP_MSG
-)
 class TestJumboFramesCase04(TestJumboFramesTestCaseBase):
     """
     Attach 4 VLAN networks over BOND to two hosts
@@ -456,10 +454,6 @@ class TestJumboFramesCase04(TestJumboFramesTestCaseBase):
         super(TestJumboFramesCase04, cls).teardown_class()
 
 
-@attr(tier=2)
-@pytest.mark.skipif(
-    conf.NOT_4_NICS_HOSTS, reason=conf.NOT_4_NICS_HOST_SKIP_MSG
-)
 class TestJumboFramesCase05(TestJumboFramesTestCaseBase):
     """
     Creates bridged VLAN network with 5000 MTU values
@@ -576,10 +570,6 @@ class TestJumboFramesCase06(TestJumboFramesTestCaseBase):
             raise conf.NET_EXCEPTION()
 
 
-@attr(tier=2)
-@pytest.mark.skipif(
-    conf.NOT_4_NICS_HOSTS, reason=conf.NOT_4_NICS_HOST_SKIP_MSG
-)
 class TestJumboFramesCase07(TestJumboFramesTestCaseBase):
     """
     Creates 2 bridged VLAN networks with different MTU and check traffic
