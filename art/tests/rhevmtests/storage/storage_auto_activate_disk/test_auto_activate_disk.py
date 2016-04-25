201623dc1d5c6292637a353d2bf9805e6fead036
Raz Tamir <ratamir@redhat.com>
2016-04-25 19:00:17 +0300

    Storage: Auto activate disk plan with fixtures
     Add pytest fixtures
    
    Change-Id: If3284fe85daade5186ad8f8f63eb0fb230c3e7f3
    Reviewed-on: https://code.engineering.redhat.com/gerrit/72985
    Tested-by: Raz Tamir <ratamir@redhat.com>
    Reviewed-by: Gilad Lazarovich <glazarov@redhat.com>
    Reviewed-by: Petr Balogh <pbalogh@redhat.com>
    Reviewed-by: Raz Tamir <ratamir@redhat.com>

--- art/tests/rhevmtests/storage/storage_auto_activate_disk/test_auto_activate_disk.py
+++ art/tests/rhevmtests/storage/storage_auto_activate_disk/test_auto_activate_disk.py
@@ -2,7 +2,7 @@
 Adding Disk to a VM which is not down adds a Disk that is activated tests
 """
 import logging
-
+import pytest
 import config
 import helpers
 from art.rhevm_api.tests_lib.low_level import (
@@ -21,6 +21,7 @@ logger = logging.getLogger(__name__)
 DISK_PERMUTATIONS = ll_disks.get_all_disk_permutation(
     interfaces=storage_helpers.INTERFACES
 )
+STORAGE_DOMAIN = None
 
 
 class VmWithOs(TestCase):
@@ -32,96 +33,60 @@ class VmWithOs(TestCase):
     vm_name = None
     vm_initial_disks = list()
 
-    @classmethod
-    def setup_class(cls):
+    @pytest.fixture(scope='class')
+    def initializer_class(self, request):
         """
         Select a VM from the inventory for use, ensure VirtIO-SCSI Enabled
         option is selected
         """
-        logger.info("setup class %s", cls.__name__)
-        if not ll_vms.does_vm_exist(config.VM_NAME[0]):
-            raise exceptions.VMException(
-                "VM '%s' doesn't exist" % config.VM_NAME[0]
-            )
-        logger.info("Pick VM '%s' from inventory for use", config.VM_NAME[0])
-        cls.vm_name = config.VM_NAME[0]
+        def finalizer_class():
+            """
+            Shuts down the VM used for tests
+            """
+            logger.info("teardown_class %s", self.__name__)
+            if self.vm_name:
+                ll_vms.stop_vms_safely([self.vm_name])
 
-        cls.vm_initial_disks = [d.get_id() for d in ll_vms.getVmDisks(
-            cls.vm_name)]
-
-        cls.storage_domain = ll_sd.getStorageDomainNamesForType(
-            config.DATA_CENTER_NAME, cls.storage
+        request.addfinalizer(finalizer_class)
+        global STORAGE_DOMAIN
+        logger.info("setup class %s", self.__name__)
+        STORAGE_DOMAIN = ll_sd.getStorageDomainNamesForType(
+            config.DATA_CENTER_NAME, self.storage
         )[0]
 
-    @classmethod
-    def teardown_class(cls):
+    @pytest.fixture(scope='function')
+    def initializer_VmWithOs(self, request, initializer_class):
         """
-        Shuts down the VM used for tests, restore the VirtIO-SCSI Enabled
-        option if it was updated
+        Start VM for test
         """
-        logger.info("teardown_class %s", cls.__name__)
-        if cls.vm_name:
-            ll_vms.stop_vms_safely([cls.vm_name])
-            logger.info(
-                "Restore configuration to any VM that had its VirtIO-SCSI "
-                "Enabled set to False before the start of the test run"
-            )
+        def finalizer_VmWithOs():
+            """ Stop VM for test, remove created disks """
+            ll_vms.stop_vms_safely([self.vm_name])
+            for disk in ll_vms.getVmDisks(self.vm_name):
+                if disk.get_id() not in self.vm_initial_disks:
+                    if not ll_disks.deleteDisk(
+                        True, alias="", disk_id=disk.get_id()
+                    ):
+                        TestCase.test_failed = True
+                        logger.error(
+                            "Deleting disk with ID '%s' failed", disk.get_id()
+                        )
+            ll_jobs.wait_for_jobs([config.JOB_REMOVE_DISK])
+            self.teardown_exception()
 
-    def setUp(self):
-        """ Start VM for test """
+        request.addfinalizer(finalizer_VmWithOs)
+        self.vm_name = config.VM_NAME[0]
+        self.vm_initial_disks = [
+            d.get_id() for d in ll_vms.getVmDisks(self.vm_name)
+        ]
+
         if not ll_vms.startVm(True, self.vm_name, config.VM_UP):
             raise exceptions.VMException(
                 "Could not power on VM '%s'" % self.vm_name
             )
 
-    def tearDown(self):
-        """ Stop VM for test, remove created disks """
-        ll_vms.stop_vms_safely([self.vm_name])
-        for disk in ll_vms.getVmDisks(self.vm_name):
-            if disk.get_id() not in self.vm_initial_disks:
-                if not ll_disks.deleteDisk(
-                        True, alias="", disk_id=disk.get_id()
-                ):
-                    TestCase.test_failed = True
-                    logger.error(
-                        "Deleting disk with ID '%s' failed", disk.get_id()
-                    )
-        ll_jobs.wait_for_jobs([config.JOB_REMOVE_DISK])
-        self.teardown_exception()
-
-
-@attr(tier=2)
-class TestCase4936(VmWithOs):
-    """
-    Add disks while vm is running
-    """
-    __test__ = True
-    polarion_test_case = "4936"
-
-    @polarion("RHEVM3-4936")
-    def test_attach_new_disk_while_running(self):
-        """
-        Attach different types of disks while the vm is running
-        """
-        for permutation in DISK_PERMUTATIONS:
-            logger.info(
-                "Adding disk %s %s %s", permutation["interface"],
-                permutation["sparse"], permutation["format"]
-            )
-            helpers.attach_new_disk(
-                self.polarion_test_case, self.vm_name, True,
-                self.storage_domain, **permutation
-            )
-
-
-class VmWithAnotherDiskWhileStatus(VmWithOs):
-    """
-    Add another disk to the VM while the VM is in a specific state
-    """
-    __test__ = False
-
     def attach_new_disk_while_status(
-            self, action, status, activate_expected_status=True, **permutation
+        self, action, status, activate_expected_status=True, **permutation
     ):
         """
         Try to add a new disk while OS is in requested state
@@ -141,12 +106,38 @@ class VmWithAnotherDiskWhileStatus(VmWithOs):
 
         assert helpers.attach_new_disk(
             self.polarion_test_case, self.vm_name, activate_expected_status,
-            self.storage_domain, **permutation
+            STORAGE_DOMAIN, **permutation
         )
 
 
 @attr(tier=2)
-class TestCase4937(VmWithAnotherDiskWhileStatus):
+@pytest.mark.usefixtures("initializer_VmWithOs")
+class TestCase4936(VmWithOs):
+    """
+    Add disks while vm is running
+    """
+    __test__ = True
+    polarion_test_case = "4936"
+
+    @polarion("RHEVM3-4936")
+    def test_attach_new_disk_while_running(self):
+        """
+        Attach different types of disks while the vm is running
+        """
+        for permutation in DISK_PERMUTATIONS:
+            logger.info(
+                "Adding disk %s %s %s", permutation["interface"],
+                permutation["sparse"], permutation["format"]
+            )
+            helpers.attach_new_disk(
+                self.polarion_test_case, self.vm_name, True,
+                STORAGE_DOMAIN, **permutation
+            )
+
+
+@attr(tier=2)
+@pytest.mark.usefixtures("initializer_VmWithOs")
+class TestCase4937(VmWithOs):
     """
     Add disks while VM is in a certain state
     """
