c12528ee66c8e14d779fc1ccf3128617bc5006ad
Meni Yakove <myakove@redhat.com>
2016-05-07 20:47:13 +0300

    Pytest: add mark 'incremental'
    xfail all tests if previous test failed and class have 'incremental' mark
    
    Change-Id: I222873d9e7e3d135c6a6eb25784db7a3d0cf4f8a
    Reviewed-on: https://code.engineering.redhat.com/gerrit/73853
    Tested-by: Meni Yakove <myakove@redhat.com>
    Reviewed-by: Artyom Lukianov <alukiano@redhat.com>
    Reviewed-by: Tareq Alayan <talayan@redhat.com>
    Reviewed-by: Meni Yakove <myakove@redhat.com>

--- pytest_customization/_pytest_art/marks.py
+++ pytest_customization/_pytest_art/marks.py
@@ -233,3 +233,17 @@ def pytest_configure(config):
 
     if config.pluginmanager.hasplugin('junitxml'):
         config.pluginmanager.register(JunitExtension(config))
+
+
+def pytest_runtest_makereport(item, call):
+    if "incremental" in item.keywords:
+        if call.excinfo is not None:
+            parent = item.parent
+            parent._previousfailed = item
+
+
+def pytest_runtest_setup(item):
+    if "incremental" in item.keywords:
+        previousfailed = getattr(item.parent, "_previousfailed", None)
+        if previousfailed is not None:
+            pytest.xfail("previous test failed (%s)" % previousfailed.name)
