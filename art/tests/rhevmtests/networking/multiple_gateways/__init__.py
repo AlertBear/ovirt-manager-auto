17666a9abda8d27475dadde6f86fe80c15e466d5
Meni Yakove <myakove@redhat.com>
2016-06-18 22:29:04 +0300

    Network: refactor_multiple_gateways
    Change-Id: If73b18f148b88161b3c5f593aeef5ddf5e11a612

--- art/tests/rhevmtests/networking/multiple_gateways/__init__.py
+++ art/tests/rhevmtests/networking/multiple_gateways/__init__.py
@@ -4,31 +4,3 @@
 """
 Multiple gateways feature Init
 """
-
-import logging
-import config as conf
-import rhevmtests.networking as networking
-import rhevmtests.networking.helper as net_helper
-
-
-logger = logging.getLogger("Multiple_Gateway_Init")
-
-#################################################
-
-
-def setup_package():
-    """
-    Running cleanup
-    Create dummies on host
-    """
-    conf.VDS_HOST_0 = conf.VDS_HOSTS[0]
-    networking.network_cleanup()
-    net_helper.prepare_dummies(host_resource=conf.VDS_HOST_0, num_dummy=3)
-    conf.HOST_NICS = conf.VDS_HOST_0.nics
-
-
-def teardown_package():
-    """
-    Delete dummies on host
-    """
-    net_helper.delete_dummies(host_resource=conf.VDS_HOST_0)
