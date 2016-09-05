68d2982b5d48a0e8b11a3f9e48bb70d80a60032f
Petr Balogh <pbalogh@redhat.com>
2016-09-05 12:12:06 +0200

    Matrix: fake api/storage fixtures
    Purpose of these fake fixtures are for preparation for new change of
    matrix plugin when teams will replace setup_class or SetUp unittest method
    to fixtures.
    
    See related patch:
    https://code.engineering.redhat.com/gerrit/#/c/82387/
    Ticket:
    RHEVM-2620
    
    Change-Id: Ia05eefd12b1b38cc8b22d6f24639e59d83d093cb
    Reviewed-on: https://code.engineering.redhat.com/gerrit/83413
    Tested-by: Petr Balogh <pbalogh@redhat.com>
    Reviewed-by: Lukas Bednar <lbednar@redhat.com>

--- setup_pytest.py
+++ setup_pytest.py
@@ -14,6 +14,7 @@ if __name__ == "__main__":
             'pytest11': [
                 'artlib = _pytest_art.artlib',
                 'artmatrix = _pytest_art.matrix',
+                'fixtures = _pytest_art.fixtures',
                 'artmarks = _pytest_art.marks',
                 'artpackagesetup = _pytest_art.package_fixtures',
                 'artautodevices = _pytest_art.autodevices',
