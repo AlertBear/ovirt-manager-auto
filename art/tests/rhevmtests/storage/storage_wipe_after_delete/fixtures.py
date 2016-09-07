fb774f84a9fc527e1cfc498757ad3d3b48abb0ab
cmestreg <cmestreg@redhat.com>
2016-09-07 13:23:13 +0100

    Storage: Move storage_wipe_after_delete to use fixtures
    Change-Id: I624ed52055f74b357a1c3c67960a0da55f07eb02
    Reviewed-on: https://code.engineering.redhat.com/gerrit/83632
    Tested-by: Carlos Mestre Gonzalez <cmestreg@redhat.com>
    Reviewed-by: Raz Tamir <ratamir@redhat.com>

---
+++ art/tests/rhevmtests/storage/storage_wipe_after_delete/fixtures.py
@@ -0,0 +1,36 @@
+import pytest
+import config
+
+from art.rhevm_api.tests_lib.low_level import (
+    storagedomains as ll_sd,
+    vms as ll_vms,
+)
+
+
+@pytest.fixture()
+def update_storage_domain_wipe_after_delete(request):
+    """
+    Enable wipe_after_delete for storage domain
+    """
+    self = request.node.cls
+
+    assert ll_sd.updateStorageDomain(
+        True, self.storage_domain, wipe_after_delete=True
+    ), "Error editing storage domain %s" % self.storage_domain
+
+
+@pytest.fixture()
+def add_disk_start_vm(request):
+    """
+    Add disk with wipe_after_delete and start the vm
+    """
+    self = request.node.cls
+
+    assert ll_vms.addDisk(
+        True, self.vm_name, config.DISK_SIZE,
+        storagedomain=self.storage_domain, sparse=True,
+        wipe_after_delete=True, interface=config.VIRTIO,
+        alias=self.disk_name
+    ), "Error adding disk to vm %s" % self.vm_name
+    ll_vms.start_vms([self.vm_name], 1, wait_for_ip=False)
+    ll_vms.waitForVMState(self.vm_name)
--- art/tests/rhevmtests/storage/storage_wipe_after_delete/test_wipe_after_delete.py
+++ art/tests/rhevmtests/storage/storage_wipe_after_delete/test_wipe_after_delete.py
@@ -5,6 +5,7 @@ https://polarion.engineering.redhat.com/polarion/#/project/RHEVM3/wiki/
 Storage/3_6_Storage_Wipe_After_Delete
 """
 import logging
+import pytest
 import threading
 from art.test_handler import exceptions
 from art.rhevm_api.tests_lib.low_level import (
@@ -20,45 +21,19 @@ from art.unittest_lib.common import StorageTest as BaseTestCase, testflow
 from art.test_handler.tools import polarion
 from art.unittest_lib import attr
 from rhevmtests.storage.storage_wipe_after_delete import config
-from rhevmtests.storage.helpers import create_vm_or_clone
 from art.test_handler.settings import opts
+from fixtures import (
+    update_storage_domain_wipe_after_delete, add_disk_start_vm
+)
+from rhevmtests.storage.fixtures import (
+    create_vm
+)
 
 logger = logging.getLogger(__name__)
 FILE_TO_WATCH = config.VDSM_LOG
 TASK_TIMEOUT = 120
-VM_NAMES = dict()
 ISCSI = config.STORAGE_TYPE_ISCSI
 FCP = config.STORAGE_TYPE_FCP
-
-
-def setup_module():
-    """
-    Sets up the environment - creates vms with all disk types and formats
-    """
-    global VM_NAMES
-    for storage_type in config.STORAGE_SELECTOR:
-        storage_domain = ll_sd.getStorageDomainNamesForType(
-            config.DATA_CENTER_NAME, storage_type
-        )[0]
-
-        vm_name = config.VM_NAME % storage_type
-        VM_NAMES[storage_type] = vm_name
-        args = config.create_vm_args.copy()
-        args['storageDomainName'] = storage_domain
-        args['vmName'] = vm_name
-
-        logger.info('Creating vm %s and installing OS on it', vm_name)
-        if not create_vm_or_clone(**args):
-            raise exceptions.VMException("Unable to create vm %s" % vm_name)
-
-
-def teardown_module():
-    """
-    Clean datacenter
-    """
-    if not ll_vms.safely_remove_vms(VM_NAMES.values()):
-        raise exceptions.VMException("Failed to remove vms in teardown")
-    ll_jobs.wait_for_jobs([config.JOB_REMOVE_VM])
 
 
 class CommonUsage(BaseTestCase):
@@ -66,26 +41,6 @@ class CommonUsage(BaseTestCase):
     Base class
     """
     __test__ = False
-    vm_name = None
-    disk_id = None
-
-    def setUp(self):
-        self.vm_name = VM_NAMES[self.storage]
-        self.storage_domain = ll_sd.getStorageDomainNamesForType(
-            config.DATA_CENTER_NAME, self.storage)[0]
-
-    def tearDown(self):
-        if ll_disks.checkDiskExists(True, self.disk_id, 'id'):
-            ll_vms.stop_vms_safely([self.vm_name])
-            ll_vms.waitForVMState(self.vm_name, config.VM_DOWN)
-            if not ll_vms.removeDisk(
-                True, self.vm_name, disk_id=self.disk_id
-            ):
-                logger.error(
-                    "Failed to remove disk attached to vm %s", self.vm_name
-                )
-                BaseTestCase.test_failed = True
-        BaseTestCase.teardown_exception()
 
     def _perform_operation(self, update=True, wipe_after_delete=False):
         """
@@ -151,6 +106,9 @@ class CommonUsage(BaseTestCase):
             )
 
 
+@pytest.mark.usefixtures(
+    create_vm.__name__,
+)
 @attr(tier=2)
 class TestCase5116(CommonUsage):
     """
@@ -185,7 +143,7 @@ class TestCase5116(CommonUsage):
 
         self.disk_id = [
             d.get_id() for d in ll_vms.getVmDisks(self.vm_name) if not
-            ll_vms.is_bootable_disk(self.vm, d.get_id())
+            ll_vms.is_bootable_disk(self.vm_name, d.get_id())
         ][0]
 
         assert ll_disks.updateDisk(
@@ -194,6 +152,9 @@ class TestCase5116(CommonUsage):
         )
 
 
+@pytest.mark.usefixtures(
+    create_vm.__name__,
+)
 @attr(tier=2)
 class TestCase10443(CommonUsage):
     """
@@ -219,10 +180,10 @@ class TestCase10443(CommonUsage):
         """
         self._perform_operation(False, True)
 
-    def tearDown(self):
-        logger.info("Test finished")
 
-
+@pytest.mark.usefixtures(
+    create_vm.__name__,
+)
 @attr(tier=1)
 class TestCase5113(CommonUsage):
     """
@@ -244,10 +205,10 @@ class TestCase5113(CommonUsage):
         """
         self._perform_operation(True)
 
-    def tearDown(self):
-        logger.info("Test finished")
 
-
+@pytest.mark.usefixtures(
+    create_vm.__name__,
+)
 @attr(tier=2)
 class TestCase5115(CommonUsage):
     """
@@ -269,10 +230,11 @@ class TestCase5115(CommonUsage):
         """
         self._perform_operation(False)
 
-    def tearDown(self):
-        logger.info("Test finished")
 
-
+@pytest.mark.usefixtures(
+    create_vm.__name__,
+    add_disk_start_vm.__name__
+)
 @attr(tier=2)
 class TestCase11864(CommonUsage):
     """
@@ -280,31 +242,10 @@ class TestCase11864(CommonUsage):
     https://polarion.engineering.redhat.com/polarion/#/project/RHEVM3/wiki/
     Storage/3_6_Storage_Wipe_After_Delete
     """
-    # Bugzilla history:
-    # 1251956 - Live storage migration is broken
-    # 1259785 - after live migrate a Virtio RAW disk, job
-    # stays in status STARTED
-    # 1292509 - It is possible to edit a disk using the api during LSM
-    # except the snapshot operation phase
     __test__ = ISCSI in opts['storages']
     storages = set([ISCSI])
     polarion_test_case = '11864'
     disk_name = "disk_%s" % polarion_test_case
-
-    def setUp(self):
-        """
-        Prepares disk with wipe_after_delete=True for VM
-        """
-        super(TestCase11864, self).setUp()
-        assert ll_vms.addDisk(
-            True, self.vm_name, config.DISK_SIZE,
-            storagedomain=self.storage_domain, sparse=True,
-            wipe_after_delete=True, interface=config.VIRTIO,
-            alias=self.disk_name
-        )
-
-        ll_vms.start_vms([self.vm_name], 1, wait_for_ip=False)
-        ll_vms.waitForVMState(self.vm_name)
 
     @polarion("RHEVM3-11864")
     def test_live_migration_wipe_after_delete(self):
@@ -339,6 +280,10 @@ class TestCase11864(CommonUsage):
             raise exceptions.DiskException("Disk update should be blocked")
 
 
+@pytest.mark.usefixtures(
+    create_vm.__name__,
+    update_storage_domain_wipe_after_delete.__name__,
+)
 @attr(tier=2)
 class TestCase10432(CommonUsage):
     """
@@ -349,17 +294,6 @@ class TestCase10432(CommonUsage):
     __test__ = ISCSI in opts['storages']
     storages = set([ISCSI])
     polarion_test_case = '10432'
-    sd_args = dict()
-
-    def setUp(self):
-        """
-        Update storage domain wipe after delete flag
-        """
-        super(TestCase10432, self).setUp()
-        self.sd_args['wipe_after_delete'] = True
-        ll_sd.updateStorageDomain(
-            True, self.storage_domain, **self.sd_args
-        )
 
     @polarion("RHEVM3-10432")
     def test_domain_configured_with_wipe_after_delete(self):
@@ -374,6 +308,3 @@ class TestCase10432(CommonUsage):
             - Operation should succeed
         """
         self._perform_operation(False)
-
-    def tearDown(self):
-        logger.info("Test finished")
