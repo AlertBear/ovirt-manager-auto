6be0c98e466078cdc264fa2f9df8ba205f638071
pmatyas <pmatyas@redhat.com>
2016-11-07 09:20:49 +0100

    Fix soft fencing to run on GE
    Change-Id: Icdd9f5a9e1d2f184730c9112a5a308826b17d5d4
    Reviewed-on: https://code.engineering.redhat.com/gerrit/89238
    Reviewed-by: Gonzalo Rafuls <grafuls@redhat.com>
    Reviewed-by: Aleksei Slaikovskii <aslaikov@redhat.com>
    Tested-by: Petr Matyas <pmatyas@redhat.com>
    Reviewed-by: Lukas Svaty <lsvaty@redhat.com>
    (cherry picked from commit 3ee176b9fb29b6d00e19bbe6513aec774f8c2120)
    Reviewed-on: https://code.engineering.redhat.com/gerrit/89239
    Reviewed-by: Kobi Hakimi <khakimi@redhat.com>

--- art/tests/rhevmtests/system/soft_fencing/__init__.py
+++ art/tests/rhevmtests/system/soft_fencing/__init__.py
@@ -2,12 +2,8 @@ import logging
 from rhevmtests.system.soft_fencing import config
 import art.test_handler.exceptions as errors
 import art.rhevm_api.tests_lib.high_level.hosts as hl_hosts
-import art.rhevm_api.tests_lib.low_level.datacenters as ll_datacenters
-import art.rhevm_api.tests_lib.high_level.datacenters as hl_datacenters
 
 logger = logging.getLogger("Soft_Fencing")
-
-#################################################
 
 
 def setup_package():
@@ -16,13 +12,6 @@ def setup_package():
     """
     config.host_with_pm = config.HOSTS[0]
     config.host_without_pm = config.HOSTS[1]
-    logger.info("Building setup...")
-    hl_datacenters.build_setup(
-        config.PARAMETERS,
-        config.PARAMETERS,
-        config.STORAGE_TYPE,
-        config.TEST_NAME
-    )
     agent = {
         "agent_type": config.PM_TYPE_IPMILAN,
         "agent_address": config.PM_ADDRESS,
@@ -42,12 +31,5 @@ def teardown_package():
     Cleans the environment
     """
     logger.info("Teardown...")
-    logger.info("Wait until datacenter up")
-    if not ll_datacenters.waitForDataCenterState(config.DC_NAME[0]):
-        logger.error("Datacenter down")
-    if not hl_datacenters.clean_datacenter(
-            positive=True, datacenter=config.DC_NAME[0],
-            vdc=config.VDC_HOST,
-            vdc_password=config.VDC_ROOT_PASSWORD
-    ):
-        logger.error("Cannot remove setup")
+    if not hl_hosts.remove_power_management(host_name=config.host_with_pm):
+        logger.error("Cannot remove power management")
