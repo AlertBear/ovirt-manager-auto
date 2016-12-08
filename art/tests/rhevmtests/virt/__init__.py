ea03cf953dd3f7a3e537932f8f3d45c7f148f82e
ipinto <ipinto@redhat.com>
2016-12-08 11:16:02 +0200

    Virt: update 'remove_all_vms_from_cluster' to high level
    Change-Id: I2bce3080da06a9bb42d41a654986292cb98a6cb5
    Reviewed-on: https://code.engineering.redhat.com/gerrit/92460
    Tested-by: Israel Pinto <ipinto@redhat.com>
    Reviewed-by: Artyom Lukianov <alukiano@redhat.com>
    Reviewed-by: Kobi Hakimi <khakimi@redhat.com>

--- art/tests/rhevmtests/virt/__init__.py
+++ art/tests/rhevmtests/virt/__init__.py
@@ -1,6 +1,7 @@
 import logging
 from art.rhevm_api.utils.inventory import Inventory
 import art.rhevm_api.tests_lib.low_level.vms as ll_vms
+import art.rhevm_api.tests_lib.high_level.vms as hl_vms
 import config
 import helper
 
@@ -27,7 +28,7 @@ def setup_package():
         ll_vms.stop_vms_safely(none_ge_vms_in_cluster)
         for vm in none_ge_vms_in_cluster:
             ll_vms.updateVm(positive=True, vm=vm, protected=False)
-    ll_vms.remove_all_vms_from_cluster(config.CLUSTER_NAME[0], skip=skip_vms)
+    hl_vms.remove_all_vms_from_cluster(config.CLUSTER_NAME[0], skip=skip_vms)
     for vm in config.VM_NAME:
         logger.info("Update VM %s to default parameters ")
         if not ll_vms.updateVm(
