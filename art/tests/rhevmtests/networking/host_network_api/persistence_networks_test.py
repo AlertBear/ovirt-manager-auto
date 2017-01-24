d53ae41009a9b27616b42f3c01531b96afbeb27d
Meni Yakove <myakove@redhat.com>
2017-01-24 10:31:19 +0200

    Network:host_network_api persistence_networks_test use parametrize
    Change-Id: I5b95e700897b7db611de5e79f97bc6bcaa373621
    Reviewed-on: https://code.engineering.redhat.com/gerrit/96036
    Tested-by: Meni Yakove <myakove@redhat.com>
    Reviewed-by: Ori Ben Sasson <obensass@redhat.com>
    Reviewed-by: Artyom Lukianov <alukiano@redhat.com>

--- art/tests/rhevmtests/networking/host_network_api/persistence_networks_test.py
+++ art/tests/rhevmtests/networking/host_network_api/persistence_networks_test.py
@@ -11,6 +11,7 @@ import rhevmtests.networking.config as conf
 import rhevmtests.networking.host_network_qos.helper as qos_helper
 import rhevmtests.networking.multi_host.helper as multi_host_helper
 from art.unittest_lib import attr, NetworkTest, testflow
+from art.test_handler.tools import polarion
 from fixtures import reboot_host
 from rhevmtests.networking import helper as network_helper
 from rhevmtests.networking.fixtures import (
@@ -52,18 +53,29 @@ class TestPersistenceSetupNetworks01(NetworkTest):
     Check the VLAN, MTU and QoS are persistence on host NIC
     Check the VLAN, MTU and QoS are persistence on host BOND
     """
-    __test__ = True
-    bond = "bond10"
+    # test_01_persistence_network_on_host_nic params
     nic_net = host_network_api_conf.PERSIST_NETS[1][0]
+    nic_vlan = (
+        host_network_api_conf.PERSIST_NETS_DICT.get(nic_net).get("vlan_id")
+    )
+
+    # test_02_persistence_network_on_host_bond params
     bond_net = host_network_api_conf.PERSIST_NETS[1][1]
-    nic_vlan = host_network_api_conf.VLAN_IDS.pop(0),
-    bond_vlan = host_network_api_conf.VLAN_IDS.pop(0),
+    bond_vlan = (
+        host_network_api_conf.PERSIST_NETS_DICT.get(bond_net).get("vlan_id")
+    )
+    bond = "bond10"
+
+    # test_01 and test_02 params
     mtu_9000 = conf.MTU[0]
     qos_dict = {
         "rt": host_network_api_conf.QOS_VAL * 1000000,
         "ul": host_network_api_conf.QOS_VAL * 1000000,
         "ls": host_network_api_conf.QOS_VAL
     }
+
+    # setup_networks_fixture params
+    persist = True
     hosts_nets_nic_dict = {
         0: {
             nic_net: {
@@ -80,55 +92,37 @@ class TestPersistenceSetupNetworks01(NetworkTest):
         }
     }
 
-    def test_01_persistence_network_on_host_nic(self):
+    @pytest.mark.parametrize(
+        ("network", "nic", "vlan"),
+        [
+            polarion("RHEVM3-19133")([nic_net, 1, nic_vlan]),
+            polarion("RHEVM3-19134")([bond_net, bond, bond_vlan])
+        ]
+    )
+    def test_host_persistence_networks(self, network, nic, vlan):
         """
-        Check the VLAN, MTU and QoS are persistence on host NIC
+        Check the VLAN, MTU and QoS are persistence on NIC
+        Check the VLAN, MTU and QoS are persistence on BOND
         """
-        host_nic = conf.HOST_0_NICS[1]
+        host_nic = conf.HOST_0_NICS[nic] if isinstance(nic, int) else nic
         testflow.step(
             "Check that MTU on host NIC %s is %s", host_nic, self.mtu_9000
         )
-        multi_host_helper.check_mtu(
-            net=self.nic_net, mtu=self.mtu_9000, nic=host_nic,
+        assert multi_host_helper.check_mtu(
+            net=network, mtu=self.mtu_9000, nic=host_nic,
             host=conf.HOST_0_NAME, vds_host=conf.VDS_0_HOST
         )
         testflow.step(
-            "Check that VLAN on host NIC %s is %s", host_nic, self.nic_vlan
+            "Check that VLAN on host NIC %s is %s", host_nic, vlan
         )
-        multi_host_helper.check_vlan(
-            net=self.nic_net, vlan=self.nic_vlan, nic=host_nic,
+        assert multi_host_helper.check_vlan(
+            net=network, vlan=vlan, nic=host_nic,
             host=conf.HOST_0_NAME, vds_host=conf.VDS_0_HOST
         )
         testflow.step(
             "Check that QOS on host NIC %s is %s", host_nic, self.qos_dict
         )
         assert qos_helper.cmp_qos_with_vdscaps(
-            host_resource=conf.VDS_0_HOST, net=self.nic_net,
-            qos_dict=self.qos_dict
-        )
-
-    def test_02_persistence_network_on_host_bond(self):
-        """
-        Check the VLAN, MTU and QoS are persistence on host BOND
-        """
-        testflow.step(
-            "Check that MTU on host NIC %s is %s", self.bond, self.mtu_9000
-        )
-        multi_host_helper.check_mtu(
-            net=self.bond_net, mtu=self.mtu_9000, nic=self.bond,
-            host=conf.HOST_0_NAME, vds_host=conf.VDS_0_HOST
-        )
-        testflow.step(
-            "Check that VLAN on host NIC %s is %s", self.bond, self.bond_vlan
-        )
-        multi_host_helper.check_vlan(
-            net=self.bond_net, vlan=self.bond_vlan, nic=self.bond,
-            host=conf.HOST_0_NAME, vds_host=conf.VDS_0_HOST
-        )
-        testflow.step(
-            "Check that QOS on host NIC %s is %s", self.bond, self.qos_dict
-        )
-        assert qos_helper.cmp_qos_with_vdscaps(
-            host_resource=conf.VDS_0_HOST, net=self.nic_net,
+            host_resource=conf.VDS_0_HOST, net=network,
             qos_dict=self.qos_dict
         )
