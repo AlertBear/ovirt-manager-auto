63424c3efc7ba1e9a1b81363303156ca19f700f0
Raz Tamir <ratamir@redhat.com>
2017-05-25 14:51:47 +0300

    Storage: Add bz to ovf TP
     https://bugzilla.redhat.com/show_bug.cgi?id=1454821
    
    Change-Id: Ie0ce63dc747b373fd94396e721dc04fe716b15dc
    Reviewed-on: https://code.engineering.redhat.com/gerrit/107339
    Tested-by: Raz Tamir <ratamir@redhat.com>
    Reviewed-by: Carlos Mestre Gonzalez <cmestreg@redhat.com>
    Reviewed-by: Raz Tamir <ratamir@redhat.com>

--- art/tests/rhevmtests/storage/storage_ovf_on_any_domain/test_ovf_on_any_domain.py
+++ art/tests/rhevmtests/storage/storage_ovf_on_any_domain/test_ovf_on_any_domain.py
@@ -24,7 +24,7 @@ from art.rhevm_api.tests_lib.low_level import (
 from art.rhevm_api.utils import test_utils
 from art.test_handler import exceptions
 from art.test_handler.settings import opts
-from art.test_handler.tools import polarion
+from art.test_handler.tools import bz, polarion
 from art.unittest_lib import attr, StorageTest as BaseTestCase
 from rhevmtests.storage.helpers import (
     create_vm_or_clone, get_spuuid, get_sduuid, get_imguuid, get_voluuid,
@@ -1449,6 +1449,7 @@ class TestCase6262(EnvironmentWithNewVm):
 
     @polarion("RHEVM3-6262")
     @attr(tier=3)
+    @bz({'1454821': {}})
     def test_ovf_for_a_template(self):
         """ Polarion case 6262 """
         self.create_and_attach_disk(VM3_NAME, self.storage_domain_0,
