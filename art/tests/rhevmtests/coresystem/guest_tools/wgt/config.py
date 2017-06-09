09a1d958e163e7eccb3358facfca695cf0d0b5fe
pmatyas <pmatyas@redhat.com>
2017-06-09 12:19:34 +0200

    Workaround for WGT on 4.2
    Change-Id: Iff634464a932a7e19c2f581e731cc7c23fd36958
    Reviewed-on: https://code.engineering.redhat.com/gerrit/108664
    Tested-by: Petr Matyas <pmatyas@redhat.com>
    Reviewed-by: Aleksei Slaikovskii <aslaikov@redhat.com>
    Reviewed-by: Michal Ovciarik <movciari@redhat.com>
    Reviewed-by: Lukas Svaty <lsvaty@redhat.com>

--- art/tests/rhevmtests/coresystem/guest_tools/wgt/config.py
+++ art/tests/rhevmtests/coresystem/guest_tools/wgt/config.py
@@ -9,6 +9,6 @@ PRODUCT = 'rhevm'
 RHEVM_VERSION = re.search("^\d+.\d+", PRODUCT_BUILD).group(0)
 RHEL_VERSION = re.search("^\d+", ENGINE_HOST.os.distribution.version).group(0)
 REPO = "http://bob.eng.lab.tlv.redhat.com/builds/latest_%s/el%s/noarch/" % (
-    RHEVM_VERSION, RHEL_VERSION
+    "4.1", RHEL_VERSION  # TODO: fix this when there is WGT in 4.2
 )
 WESTMERE_CL_CPU_LVL = "Intel Westmere Family"
