b737a849a94fdfe7dbc16ca369cc28133fa2a12f
Kobi Hakimi <khakimi@redhat.com>
2017-07-16 15:12:37 +0300

    Remove redundant HE details from config file
    Since we don't use these hosted engine parameters it will be
    better to remove them.
    
    Change-Id: I4a9c2ea57f8430a4b4daa14eb4f9cf7045f6e3d6
    Reviewed-on: https://code.engineering.redhat.com/gerrit/112388
    Tested-by: Kobi Hakimi <khakimi@redhat.com>
    Reviewed-by: Katerina Koukiou <kkoukiou@redhat.com>
    Reviewed-by: Kobi Hakimi <khakimi@redhat.com>

--- art/tests/infra/golden_env/cleaner/config.py
+++ art/tests/infra/golden_env/cleaner/config.py
@@ -29,19 +29,6 @@ ENGINE = resources.Engine(
 HOSTED_ENGINE = ART_CONFIG.get('HOSTED_ENGINE')
 
 # Hosted engine parameters
-HE_HOST_IP = GE['hosts'][0]['address']
-HE_ADDITIONAL_HOSTS = [host['address'] for host in GE['hosts'][1:]]
 HE_VM_NAME = 'HostedEngine'
 HE_SD_NAME = 'hosted_storage'
 HE_CL_NAME = GE['clusters'][0]['name']
-HE_DC_NAME = GE['datacenters'][0]['name']
-HE_SD_TYPE = (
-    GE.get('hosted_engine_details', {}).get('storage', {}).get('type')
-)
-HE_SD_ADDRESS = (
-    GE.get('hosted_engine_details', {}).get('storage', {}).get('address')
-)
-HE_SD_LUN = (
-    GE.get('hosted_engine_details', {}).get('storage', {}).get('lun')
-)
-HE_SD_PATH = GE.get('hosted_engine_details', {}).get('storage', {}).get('path')
