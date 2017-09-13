1f172bb1141e0b09d813b62dd12c1f4d3ecf356e
Meni Yakove <myakove@redhat.com>
2017-09-13 15:07:03 +0300

    Network: LLDP WA for BZ 1487930
    Cisco have a bug in thier switches and LLDP is sent on taged traffix
    with VLAN 1.
    We need to add VLAN 1 to the host NIC to get LLDP info.
    
    Change-Id: I70ad43b0ae23aad696ed7b1e0614608bb8f7c588
    Reviewed-on: https://code.engineering.redhat.com/gerrit/117983
    Tested-by: Meni Yakove <myakove@redhat.com>
    Reviewed-by: Ori Ben Sasson <obensass@redhat.com>
    Reviewed-by: Tareq Alayan <talayan@redhat.com>

--- art/tests/rhevmtests/networking/lldp/lldp_test.py
+++ art/tests/rhevmtests/networking/lldp/lldp_test.py
@@ -1,30 +1,81 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import pytest
+
 import art.rhevm_api.tests_lib.low_level.hosts as ll_hosts
 import rhevmtests.networking.config as conf
 from art.unittest_lib import NetworkTest, tier2
-from art.test_handler.tools import polarion, bz
+from art.core_api import apis_utils
+from art.test_handler.tools import polarion
+from art.core_api import apis_exceptions
+from rhevmtests.networking.fixtures import (  # noqa: F401
+    remove_all_networks,
+    setup_networks_fixture,
+    clean_host_interfaces,
+    create_and_attach_networks,
+
+)
 
 
 @tier2
 @polarion("RHEVM-22039")
-@bz(
-    {
-        "1484725": {},
-        "1487078": {},
-        "1487157": {},
-        "1487930": {}
-    }
+@pytest.mark.usefixtures(
+    create_and_attach_networks.__name__,
+    setup_networks_fixture.__name__
 )
 class TestLldp(NetworkTest):
     """
     1. Get LLDP info for host NIC
     """
+    # General params
+    lldp_net = "lldp-vlan-1-net"
+
+    # create_and_attach_networks params
+    create_networks = {
+        "1": {
+            "data_center": conf.DC_0,
+            "clusters": [conf.CL_0],
+            "networks": {
+                lldp_net: {
+                    "vlan_id": "1",
+                    "required": "false"
+                }
+            }
+        }
+    }
+
+    # remove_all_networks params
+    remove_dcs_networks = [conf.DC_0]
+
+    # setup_networks_fixture params
+    hosts_nets_nic_dict = {
+        0: {
+            lldp_net: {
+                "network": lldp_net,
+                "nic": 0
+            }
+        }
+    }
+    # clean_host_interfaces params
+    hosts_nets_nic_dict = conf.CLEAN_HOSTS_DICT
+
     def test_lldp_info(self):
         """
         Get LLDP info for host NIC
         """
-        assert ll_hosts.get_lldp_nic_info(
-            host=conf.HOST_0_NAME, nic=conf.HOST_0_NICS[0]
+        nic = conf.HOST_0_NICS[0]
+        sampler = apis_utils.TimeoutingSampler(
+            timeout=60,
+            sleep=1,
+            func=ll_hosts.get_lldp_nic_info,
+            host=conf.HOST_0_NAME,
+            nic=nic
         )
+        for sample in sampler:
+            try:
+                if not sample:
+                    continue
+                break
+            except apis_exceptions.APITimeout:
+                assert sample, "Failed to get LLDP info for %s" % nic
