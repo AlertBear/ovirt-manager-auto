24ea705ff1eb4bf705df37f267cd5bbd86b3ae39
Katerina Koukiou <kkoukiou@redhat.com>
2017-11-08 15:20:05 +0100

    RHEVM-3807: Add human readable short summary for GE state
    Summary will be printed in the start of each teams package execution.
    
    Change-Id: I4c0146d12972496020575a16ac192065f91f3457
    Reviewed-on: https://code.engineering.redhat.com/gerrit/122726
    Tested-by: Katerina Koukiou <kkoukiou@redhat.com>
    Reviewed-by: Kobi Hakimi <khakimi@redhat.com>
    Reviewed-by: Israel Pinto <ipinto@redhat.com>
    Reviewed-by: Katerina Koukiou <kkoukiou@redhat.com>

--- pytest_customization/_pytest_art/leftoversinfo.py
+++ pytest_customization/_pytest_art/leftoversinfo.py
@@ -34,6 +34,47 @@ class LeftoversInfo(object):
         from art.rhevm_api.utils.inventory import Inventory
         return Inventory()
 
+    def pretty_print_ge_state_report(self):
+        """
+        Shows GE state information in the form of tables per resource type
+        using tabulate module
+        """
+        # Filter ge_state dict and create a short GE state summary
+        short_report_attrs = ["name", "status"]
+        ignore_rsrc_names = ["OVF_STORE"]
+        short_summary = {}
+        for resource_type in self.ge_state:
+            short_summary[resource_type] = []
+            resource_obj_list = self.ge_state[resource_type]
+            for resource in resource_obj_list:
+                if ('name' in resource and
+                        resource['name'] in ignore_rsrc_names):
+                    continue
+                resource_dict = {}
+                attr_gen = (attr for attr in short_report_attrs
+                            if attr in resource)
+                for attr in attr_gen:
+                    resource_dict[attr] = resource[attr]
+                short_summary[resource_type].append(resource_dict)
+
+        from tabulate import tabulate
+        logger.info(
+            "Printing GE State before team's %s package execution", self.team
+        )
+        full_short_summary = []
+        for resource_type in short_summary:
+            full_short_summary.append(
+                "\n{}\n{}\n{}".format(
+                    resource_type.title(),
+                    len(resource_type)*"*",
+                    tabulate(
+                        short_summary[resource_type],
+                        headers="keys", tablefmt='orgtbl'
+                    )
+                )
+            )
+        logger.info("\n".join(full_short_summary))
+
     def pytest_rhv_setup(self, team):
         """
         Get dict containing GE description at package setup
@@ -48,6 +89,7 @@ class LeftoversInfo(object):
         inventory = self._get_inventory_instance()
         self.ge_state = inventory.get_summary_report()
         self.team = team
+        self.pretty_print_ge_state_report()
 
     def pytest_rhv_teardown(self, team):
         """
